808144942b4aed9571c87227d360db5e596a465e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 10:58:46 +0000

    Thu, Aug 31, 2023, 5:58 PM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -14,7 +14,12 @@ int main(void)
     long card_number = 123456789;
     int card_length = get_length(card_number);
     bool is_card_valid = check_card(card_number);
-    printf("%d\n", is_card_valid);
+    if (is_card_valid == 0)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+    if ()
 }
 
 int get_length(long card_number)
