17d65781ca041806aaf065618cc971f9183eebf8
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 22:44:45 +0000

    Fri, Sep 1, 2023, 5:44 AM +07:00

--- test/test.c
+++ test/test.c
@@ -8,5 +8,17 @@
 
 int main(int argc, string argv[])
 {
-    printf("%i\n", argc);
+    // ensure proper usage
+    // TODO #1
+    
+    if (!argv[1])
+    {
+        printf("%s\n", "Usage: ./wordle wordsize\n");
+        return 1;
+    }
+    else if (argv[1] < 4 || argv[1] > 8)
+    {
+        printf("%s\n", "Error: wordsize  must be either 5, 6, 7, or 8\n");
+        return 1;
+    }
 }
