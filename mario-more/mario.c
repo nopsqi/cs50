170f4bca1210e94164ba5bf8cabe0750bcd5a897
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 03:06:56 +0000

    Thu, Aug 31, 2023, 10:06 AM +07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n = get_int("Input number: ");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n*2; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
