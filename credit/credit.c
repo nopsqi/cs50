90667a2784294fa98bb5a8a2e8a0e2d6f9719064
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 10:14:45 +0000

    Thu, Aug 31, 2023, 5:14 PM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,9 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_length(long card_number);
+
 int main(void)
 {
     // long card_number = get_long("Input card number: ");
     long card_number = 123456789;
-    printf("%ld\n", (card_number /= 1000000000));
+    int card_length = get_length(card_number);
+    string 
+    printf("%i\n", card_length);
+}
+
+int get_length(long card_number)
+{
+    int length = 0;
+    while (card_number != 0)
+    {
+        card_number /= 10;
+        length++;
+    }
+    return length;
 }
