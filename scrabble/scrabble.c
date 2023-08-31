04070c53f673ae3e84347ff985663e6c4c42bf78
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 15:35:20 +0000

    Thu, Aug 31, 2023, 10:35 PM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,6 +8,7 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 int compute_score(string word);
 int get_index(char array[]);
+int get_size(char array[]);
 
 int main(void)
 {
@@ -25,20 +26,26 @@ int main(void)
 
 int compute_score(string word)
 {
+    // TODO: Compute and return score for string
     char capital[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    // TODO: Compute and return score for string
+    
     return 0;
 }
 
-int get_index(char array[])
+int get_index(char c, char array[])
 {
     array_size = get_size(array)
     int index = -1;
     for (int i = 0; i < array_size; i++)
     {
-        if
+        if (c == array[i])
+        {
+            index = i;
+            break;
+        }
     }
+    return index;
 
 }
 
