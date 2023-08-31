22fc2a66473aab3e3fb051e9135bc0c190df966d
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 22:26:08 +0000

    Fri, Sep 1, 2023, 5:26 AM +07:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -27,7 +27,15 @@ int main(int argc, string argv[])
 {
     // ensure proper usage
     // TODO #1
-    if (argv[1] )
+    if (!argv[1])
+    {
+        printf("%s\n", "Usage: ./wordle wordsize\n");
+        return 1;
+    }
+    else if (argv[1] < 4 || argv[1] > 8)
+    {
+        printf("%s\n", "Error: wordsize  must be either 5, 6, 7, or 8\n");
+    }
 
     int wordsize = 0;
 
