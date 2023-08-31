aa774a820db4b5675f0bb69f78da0fd4ee66c53c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 20:48:08 +0000

    Fri, Sep 1, 2023, 3:48 AM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,6 +5,7 @@
 int coleman(string text);
 int count_sentences(string text);
 int count_words(string text);
+int count_letters(string text);
 
 int main(void)
 {
@@ -19,11 +20,11 @@ int coleman(string text)
     int index = 0;
     int sentences = count_sentences(text);
     int words = count_words(text);
-    // int letters = count_letters(text);
+    int letters = count_letters(text);
     // float l = letters / words * 100
     // float s = sentences / words * 100
     // index = 0.0588 * l - 0.296 * s - 15.8
-    return words;
+    return letters;
 }
 
 int count_sentences(string text)
@@ -52,4 +53,18 @@ int count_words(string text)
         }
     }
     return counter;
+}
+
+int count_letters(string text)
+{
+    int counter = 0;
+    int length = strlen(text);
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == ' ')
+        {
+            counter++;
+        }
+    }
+    return length - counter;
 }
