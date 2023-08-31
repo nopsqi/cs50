6495195306a15ecf21e233375ded1834eb849d80
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 20:25:00 +0000

    Fri, Sep 1, 2023, 3:25 AM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,9 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int coleman(string text);
+
 int main(void)
 {
-
+    // string text = get_string("Input text: ");
+    string text = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
+    int grade = coleman(text);
 }
 
-int coleman(int l, int s)
+int coleman(string text)
+{
+    int index = 0;
+    int sentences = count_snetences();
+    int letters = count_letters();
+    int words = count_words();
+    float l = letters / words * 100
+    float s = sentences / words * 100
+    index = 0.0588 * l - 0.296 * s - 15.8
+    return index;
+}
+
+int count_sentences(stirng text)
+{
+    
+}
