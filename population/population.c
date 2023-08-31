143209bd3b0ffea3a9c1d91379a884158496254c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 02:27:17 +0000

    Thu, Aug 31, 2023, 9:27 AM +07:00

--- population/population.c
+++ population/population.c
@@ -10,7 +10,9 @@ int main(void)
     // TODO: Prompt for end size
     int end_size = get_number("End size: ");
     // TODO: Calculate number of years until we reach threshold
-
+    int year = 0;
+    int end_size_calculate = 0;
+    while (year)
     // TODO: Print number of years
 
     printf("%i %i\n", start_size, end_size);
