fba15293ffca893447a055c4191508739a12e259
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 02:20:26 +0000

    Thu, Aug 31, 2023, 9:20 AM +07:00

--- population/population.c
+++ population/population.c
@@ -1,14 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_number(void);
+int get_number(string description);
 
 int main(void)
 {
     // TODO: Prompt for start size
     int start_size = get_number("Start size: ");
     // TODO: Prompt for end size
-    printf("%i\n", start_size);
+    int end_size = get_number("End size: ");
+    printf("%i %i\n", start_size, end_size);
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
@@ -19,7 +20,7 @@ int get_number(string description)
     int n;
     do
     {
-        n = get_int(description);
+        n = get_int("%s", description);
     }
     while (n < 1);
     return n;
