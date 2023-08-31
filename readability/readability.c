0758642c87d804de7fdd383a7d6e0f9c7fd80b80
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 20:41:42 +0000

    Fri, Sep 1, 2023, 3:41 AM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,33 +4,52 @@
 
 int coleman(string text);
 int count_sentences(string text);
+int count_words(string text);
 
 int main(void)
 {
     // string text = get_string("Input text: ");
     string text = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
-    printf("%lu\n", strlen(text));
     int grade = coleman(text);
+    printf("%i\n", grade);
 }
 
 int coleman(string text)
 {
     int index = 0;
     int sentences = count_sentences(text);
+    int words = count_words(text);
     // int letters = count_letters(text);
-    // int words = count_words(text);
     // float l = letters / words * 100
     // float s = sentences / words * 100
     // index = 0.0588 * l - 0.296 * s - 15.8
-    return index;
+    return words;
 }
 
 int count_sentences(string text)
 {
+    int counter = 0;
     int length = strlen(text);
     for (int i = 0; i < length; i++)
     {
-        if (text[i] )
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            counter++;
+        }
     }
-    return 0;
+    return counter;
+}
+
+int count_words(string text)
+{
+    int counter = 0;
+    int length = strlen(text);
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == ' ')
+        {
+            counter++;
+        }
+    }
+    return counter;
 }
