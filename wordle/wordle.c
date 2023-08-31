e36b2e97ecbdb24a15c60d804f950605703b1fd3
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 22:28:20 +0000

    Fri, Sep 1, 2023, 5:28 AM +07:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -35,12 +35,14 @@ int main(int argc, string argv[])
     else if (argv[1] < 4 || argv[1] > 8)
     {
         printf("%s\n", "Error: wordsize  must be either 5, 6, 7, or 8\n");
+        return 1;
     }
 
     int wordsize = 0;
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
+    wordsize = argv[1]
 
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
