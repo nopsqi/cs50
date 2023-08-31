b309ca77fc53163a1808cb9ff9189bcb71c984ee
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 10:30:50 +0000

    Thu, Aug 31, 2023, 5:30 PM +07:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 int get_length(long card_number);
 bool check_card(long card_number);
@@ -11,10 +12,6 @@ int main(void)
     long card_number = 123456789;
     int card_length = get_length(card_number);
     bool is_card_valid = check_card(card_number);
-
-    printf("%i\n", card_length);
-    printf("%d\n", is_card_valid);
-
 }
 
 int get_length(long card_number)
@@ -31,8 +28,10 @@ int get_length(long card_number)
 bool check_card(long card_number)
 {
     int card_length = get_length(card_number);
-    for (int i = 0; i < card_length; i)
+    for (int i = 0; i < card_length; i += 2)
     {
-
+        int number_now = (card_number / pow(10, i)) % 10.0;
+        printf("%i\n", number_now);
     }
+    return true;
 }
