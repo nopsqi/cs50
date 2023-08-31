c4903b0b740193953c63419a703b38813670f1a1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-08-31 22:47:52 +0000

    Fri, Sep 1, 2023, 5:47 AM +07:00

--- test/test.c
+++ test/test.c
@@ -10,15 +10,8 @@ int main(int argc, string argv[])
 {
     // ensure proper usage
     // TODO #1
-    
-    if (!argv[1])
+    if (argc < 2 || argc > 2)
     {
-        printf("%s\n", "Usage: ./wordle wordsize\n");
-        return 1;
-    }
-    else if (argv[1] < 4 || argv[1] > 8)
-    {
-        printf("%s\n", "Error: wordsize  must be either 5, 6, 7, or 8\n");
-        return 1;
+        printf("%s\n", "Usage: ./wordle wordsize");
     }
 }
--- wordle/wordle.c
+++ wordle/wordle.c
@@ -27,6 +27,12 @@ int main(int argc, string argv[])
 {
     // ensure proper usage
     // TODO #1
+
+
+    int wordsize = 0;
+
+    // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
+    // TODO #2
     if (!argv[1])
     {
         printf("%s\n", "Usage: ./wordle wordsize\n");
@@ -37,11 +43,6 @@ int main(int argc, string argv[])
         printf("%s\n", "Error: wordsize  must be either 5, 6, 7, or 8\n");
         return 1;
     }
-
-    int wordsize = 0;
-
-    // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
-    // TODO #2
     wordsize = argv[1];
 
     // open correct file, each file has exactly LISTSIZE words
