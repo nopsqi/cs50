caeae535b0fb8dc6527be70c3de7215c6f76695c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-01 23:03:42 +0000

    Sat, Sep 2, 2023, 6:03 AM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -25,6 +25,7 @@ int main(int argc, string argv[])
     printf("%s\n", argv[1]);
     if (!check_key(argv[1]))
     {
+        // printf("Error: Input valid key (26 uppercase non repeated alphabetic character)\n");
         printf("Key must contain 26 characters.");
         return 1;
     }
@@ -41,11 +42,6 @@ bool check_key(string key)
             break;
         }
         if (!isalpha(key[k]))
-        {
-            check = false;
-            break;
-        }
-        if (islower(key[k]))
         {
             check = false;
             break;
