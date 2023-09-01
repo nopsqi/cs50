559dc395ef81a0c956b1c5df4494256df36cef85
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-01 23:08:42 +0000

    Sat, Sep 2, 2023, 6:08 AM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -59,6 +59,10 @@ bool check_key(string key)
 
 string encrypt(string plaintext, string key)
 {
+    char capital[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     for (int p = 0; p < strlen(plaintext); p++)
     {
         if (!isalpha(plaintext[p]))
