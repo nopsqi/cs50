16cf410173cab48702e848f3c37d1cd4df323b5e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-01 18:23:07 +0000

    Sat, Sep 2, 2023, 1:23 AM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,8 +3,12 @@
 #include <stdbool.h>
 #include <string.h>
 
+char capital[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 const int KEY_LENGTH = 26;
-bool check_(string key);
+bool check_alphabetic(string key);
 
 int main(int argc, string argv[])
 {
@@ -19,14 +23,12 @@ int main(int argc, string argv[])
         return 1;
     }
     printf("%s\n", argv[1]);
-    bool is_key_valid = check_(argv[1]);
+    bool is_key_valid = check_alphabetic(argv[1]);
     printf("%i\n", is_key_valid);
 }
 
-bool check_(string key)
+bool check_alphabetic(string key, char array[])
 {
-    char capital[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     bool check = true;
     for (int k = 0; k < KEY_LENGTH; k++)
     {
@@ -36,7 +38,7 @@ bool check_(string key)
         }
         for (int c = 0; c < KEY_LENGTH; c++)
         {
-            if (key[k] == capital[c])
+            if (key[k] == array[c])
             {
                 break;
             }
