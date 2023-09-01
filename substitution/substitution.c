5912ca68a65ac897d33232378f4953ba684598d4
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-01 22:17:27 +0000

    Sat, Sep 2, 2023, 5:17 AM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 char capital[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 const int KEY_LENGTH = 26;
-bool check_alphabetic(string key, char array[]);
-bool check_repeat(string key);
+bool check_key(string key);
 
 int main(int argc, string argv[])
 {
@@ -26,39 +26,9 @@ int main(int argc, string argv[])
         return 1;
     }
     printf("%s\n", argv[1]);
-    bool is_key_valid = check_alphabetic(argv[1], capital);
-    printf("%i\n", is_key_valid);
 }
 
-bool check_alphabetic(string key, char array[])
+bool check_key(string key)
 {
-    bool check = true;
-    for (int k = 0; k < KEY_LENGTH; k++)
-    {
-        if (!check)
-        {
-            break;
-        }
-        for (int c = 0; c < KEY_LENGTH; c++)
-        {
-            if (key[k] == array[c])
-            {
-                break;
-            }
-            if (c == KEY_LENGTH-1)
-            {
-                check = false;
-            }
-        }
-    }
-    return check;
-}
-
-bool check_repeat(string key)
-{
-    for (int k = 0; k < KEY_LENGTH; k++)
-    {
-        break;
-    }
-    return true;
+    
 }
