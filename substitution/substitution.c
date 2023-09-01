3accdc562db86032ec2c4eb09f79162cab0849f0
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-01 10:11:30 +0000

    Fri, Sep 1, 2023, 5:11 PM +07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    if (argc < 2 || argc > 2)
+    {
+        printf("%s\n", "Usage: ./subtition key");
+        return 1;
+    }
 }
