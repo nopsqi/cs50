f4cad6edfce84a8c09a70c9c357f1ee073f290f1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-02 04:55:13 +0000

    Sat, Sep 2, 2023, 11:55 AM +07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,6 +3,10 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+int check_position(char c);
+bool check_key(string key);
 
 int main(int argc, string argv[])
 {
@@ -11,16 +15,25 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    for (int i = 0; i < strlen(argv[1]); i++)
+    if (check_key(argv[1]))
     {
-        if (!isdigit(argv[1][i]))
-        {
-            printf("Usage: ./caesar key\n");
-            return 1;
-        }
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
     int key = strtol(argv[1], NULL, 10);
     printf("%i\n", key);
+}
+
+bool check_key(string key)
+{
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (!isdigit(key[i]))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int check_position(char c)
@@ -30,7 +43,7 @@ int check_position(char c)
     int position = -1;
     for (int i = 0; i < KEY_LENGTH; i++)
     {
-        if (c == capital[i])
+        if (toupper(c) == capital[i])
         {
             position = i;
             break;
