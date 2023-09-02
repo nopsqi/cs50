db25f1fe52111a7b4b7748b3bdb6312e61bc032a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-02 04:48:42 +0000

    Sat, Sep 2, 2023, 11:48 AM +07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,15 +8,17 @@ int main(int argc, string argv[])
 {
     if (argc < 2 || argc > 2)
     {
-        printf("%s\n", "Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
     for (int i = 0; i < strlen(argv[1]); i++)
     {
-        printf("%c ", argv[1][i]);
-        printf("%i\n", isdigit(argv[1][i]));
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
     }
-    printf("\n");
-    // int key = strtol(argv[1], NULL, 10);
-    // printf("%i\n", key);
+    int key = strtol(argv[1], NULL, 10);
+    printf("%i\n", key);
 }
