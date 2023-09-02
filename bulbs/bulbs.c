721d689b2cd92118b5f8874173af3214ea838524
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-02 12:18:42 +0000

    Sat, Sep 2, 2023, 7:18 PM +07:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -1,29 +1,40 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 const int BITS_IN_BYTE = 8;
 
 void print_bulb(int bit);
-int* decimal_to_binary(int ascii);
+int *decimal_to_binary(int ascii);
+void encrypt(string message);
 
 int main(void)
 {
     // TODO
-    string message = "HI!";
+    // string message = "HI!";
+    string message = get_string("Message: ");
     encrypt(message);
 }
 
 void encrypt(string message)
 {
-    
+    for (int i = 0; i < strlen(message); i++)
+    {
+        int *binary = decimal_to_binary((int) message[i]);
+        for (int j = 0; j < BITS_IN_BYTE; j++)
+        {
+            // printf("%i", binary[j]);
+            print_bulb(binary[j]);
+        }
+        printf("\n");
+    }
 }
 
-int* decimal_to_binary(int ascii)
+int *decimal_to_binary(int ascii)
 {
     static int result[BITS_IN_BYTE];
-    for (int i = BITS_IN_BYTE-1; i > 0; i--)
+    for (int i = BITS_IN_BYTE - 1; i > 0; i--)
     {
         // printf("%i ", ascii);
         int reminder = ascii % 2;
