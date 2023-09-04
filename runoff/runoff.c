a803326a1bb692b97dd6d2c1ef21554e397f6ea0
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-04 03:52:43 +0000

    Mon, Sep 4, 2023, 10:52 AM +07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,13 +147,10 @@ void tabulate(void)
     // TODO
     for (int i = 0; i < voter_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            int candidate_index = preferences[i][j];
-            if (candidates[candidate_index].eliminated)
-                continue;
-            candidates[candidate_index].votes++;
-        }
+        int candidate_index = preferences[i][0];
+        if (candidates[candidate_index].eliminated)
+            continue;
+        candidates[candidate_index].votes++;
     }
     return;
 }
