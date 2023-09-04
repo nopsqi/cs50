ab8a3dc3acefeadd29620a20c2a5590a754a9560
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-04 04:00:13 +0000

    Mon, Sep 4, 2023, 11:00 AM +07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -165,7 +165,7 @@ bool print_winner(void)
 {
     // TODO
     // int half_votes = (int)round((float)voter_count / 2);
-    // int half_votes = (int)round((float)voter_count / 2);
+    int half_votes = voter_count / 2;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes > half_votes)
