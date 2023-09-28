c24b1e5ad49af278b1cc0be8bc8c2f4d071c87e1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-28 07:59:31 +0000

    Thu, Sep 28, 2023, 2:59 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -125,7 +125,10 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            preferences[ranks[i]][ranks[j]]++
+        }
     }
     return;
 }
