24bd8054ce484c2d5ddde90623023d43f0ccd4fa
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-28 09:39:19 +0000

    Thu, Sep 28, 2023, 4:39 PM +07:00

--- playground/playground.c
+++ playground/playground.c
@@ -2,9 +2,30 @@
 #include <cs50.h>
 #include <string.h>
 
+int arr[5] = {3, 1, 2, 4, 1};
+void sort_arr();
+
 int main()
 {
-    string a = "book";
-    if (strcmp(a, "aaaa") == 0)
-        printf("this is true");
+    sort_arr();
+}
+
+void sort_arr()
+{
+    bool swap = true;
+    while(swap)
+    {
+        swap = false;
+        for (int i = 0; i < 4; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swap = true;
+            }
+        }
+    }
+    return;
 }
