7423004dc3a61a9804e8cef0258cc05008f15ae7
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-28 23:53:21 +0000

    Fri, Sep 29, 2023, 6:53 AM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -224,7 +224,7 @@ void print_winner(void)
             row_sum += locked[i][j];
             column_sum += locked[j][i];
         }
-        if (column_sum == 0)
+        if (row_sum == candidate_count-1)
         {
             // sum_checker = row_sum;
             winner_index = i;
