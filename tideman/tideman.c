fa56b43ea0e4d00df753d6302de591e218bb292a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-28 10:48:47 +0000

    Thu, Sep 28, 2023, 5:48 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -184,7 +184,13 @@ bool check_locked(int i, int j)
 {
     if (i > candidate_count)
         return;
-    for ()
+    for (int k = 0; k < candidate_count; k++)
+    {
+        if (locked[i][k])
+        {
+            
+        }
+    }
 }
 
 // Lock pairs into the candidate graph in order, without creating cycles
