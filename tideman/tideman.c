215776acc878a3a41a780252f27246545abbe33c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-09-28 09:52:53 +0000

    Thu, Sep 28, 2023, 4:52 PM +07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -168,7 +168,7 @@ void sort_pairs(void)
         swap = false;
         for (int i = 0; i < pair_count-1; i++)
         {
-            if (preferences[pairs[i].winner][pairs[i].loser] > preferences[pairs[i+1].winner][pairs[i+1].loser])
+            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[i+1].winner][pairs[i+1].loser])
             {
                 pair temp = pairs[i];
                 pairs[i] = pairs[i+1];
