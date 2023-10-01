cf0051278c299abcfc1a6944d021e03c4c906758
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-01 13:00:25 +0000

    Sun, Oct 1, 2023, 8:00 PM +07:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,11 +40,12 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer = 0;
-    while(buffer != EOF)
+    while(buffer != -5)
     {
         int16_t tmp = buffer * factor;
         fread(&buffer, sizeof(int16_t), 1, input);
-        fwrite(&buffer, sizeof(int16_t), 1, output);
+        // fwrite(&buffer, sizeof(int16_t), 1, output);
+        printf("%i\n", buffer);
     }
 
     // Close files
