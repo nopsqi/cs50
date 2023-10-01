4cf11d022ef9208aef1329d4b74306e46e1761e5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-01 12:36:24 +0000

    Sun, Oct 1, 2023, 7:36 PM +07:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,8 +39,8 @@ int main(int argc, char *argv[])
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    char buffer;
-    while((buffer = fgetc(input)) != EOF)
+    int buffer;
+    while((buffer = fgetc(input)) != (int)EOF)
     {
         printf("%i ", buffer);
     }
