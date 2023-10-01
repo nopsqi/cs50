d189f4da7ed2b06cf9b3c3fe1cb471825aa47205
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-01 11:56:24 +0000

    Sun, Oct 1, 2023, 6:56 PM +07:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,6 +41,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     
+
     // Close files
     fclose(input);
     fclose(output);
