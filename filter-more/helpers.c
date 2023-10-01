dc52dc205b0579459bcfa9af096a4c55993f8509
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-01 17:51:54 +0000

    Mon, Oct 2, 2023, 12:51 AM +07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -25,7 +25,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
         RGBTRIPLE (*row)[width] = malloc(width * sizeof(RGBTRIPLE));
-        row = image[i];
+        *row[0] = image[i][0];
         if (i < 5)
             printf("%p %p\n", image, row);
 
