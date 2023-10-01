6cde5e4cc7576d2b5c26d17e40df3672b12063b2
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-01 10:07:58 +0000

    Sun, Oct 1, 2023, 5:07 PM +07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,4 +3,11 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    for (int i; i < height; i++)
+    {
+        for (int j; j < width; j++)
+        {
+            
+        }
+    }
 }
