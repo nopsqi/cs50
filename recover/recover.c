5a81efe37e1354250151879fddd6bf66edca456b
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-02 21:35:44 +0000

    Tue, Oct 3, 2023, 4:35 AM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,7 +17,8 @@ int main(int argc, char *argv[])
 
     while (fread(&buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-        printf("f)
+        printf("%x\n", buffer);
+        break;
     }
 }
 
