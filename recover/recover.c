627537f171851d8795bb0835042daf549db54136
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-02 21:40:42 +0000

    Tue, Oct 3, 2023, 4:40 AM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -13,13 +13,16 @@ int main(int argc, char *argv[])
         return 1;
     }
     FILE *file = fopen(argv[1], "r");
-    uint8_t *buffer = malloc(BLOCK_SIZE);
+    uint8_t *buffer = malloc(BLOCK_SIZE * sizeof(uint8_t));
 
     while (fread(&buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-        printf("%x\n", buffer);
+        printf("%s\n", buffer);
         break;
     }
+
+    fclose(file);
+    free(buffer);
 }
 
 bool isJpeg()
