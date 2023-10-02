2e562e69ab236177281de78aecd7976998a12e22
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-02 21:22:37 +0000

    Tue, Oct 3, 2023, 4:22 AM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCK_SZE 512;
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
         printf("Usage: ./recover IMAGE");
         return 1;
+    }
+    char *filename = argv[1];
+
+    while (fread(buffer, 1, BLOCK_SIZE, filename) == BLOCK_SIZE)
+    {
+        
     }
 }
