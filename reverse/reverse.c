4d0dde205cfb82d666ffd5fda423cb4f28a406c3
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-03 14:01:07 +0000

    Tue, Oct 3, 2023, 9:01 PM +07:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 
     // Write reversed audio to file
     // TODO #8
-    uint16_t *data = malloc(sizeof(block_size));
+    uint8_t *data = malloc(sizeof(block_size));
     while(fread(data, 1, block_size, input) == block_size)
     {
         for (int i = 0; i < block_size; i++)
