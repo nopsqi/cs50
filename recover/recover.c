c4a97f3283e598ff04d6e1076ba0ffdf4b315a48
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-03 10:08:47 +0000

    Tue, Oct 3, 2023, 5:08 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,7 +32,10 @@ int main(int argc, char *argv[])
             image_counter++;
         }
         if (image_counter > 0)
+        {
             fwrite(buffer, 1, BLOCK_SIZE, outfile);
+            printf("writing on %i\n", image_counter);
+        }
     }
     printf("total = %i\n", image_counter);
 
