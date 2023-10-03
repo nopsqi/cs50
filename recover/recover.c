02b797c6008f09cc6a9ac89ec4a225db5bc41647
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-03 09:34:33 +0000

    Tue, Oct 3, 2023, 4:34 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
     {
         if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) && (buffer[3] >= 0xe0 && buffer[3] <= 0xef))
         {
+            char 
             FILE *outfile = fopen()
             outname++;
         }
