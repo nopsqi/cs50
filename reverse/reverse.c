e1676788be556479bc740b214dd1c6a3abc2fe0d
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-03 20:59:29 +0000

    Wed, Oct 4, 2023, 3:59 AM +07:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
     while(ftell(input) > 43)
     {
         printf("%li\n", ftell(output));
-        fseek()
+        fseek(input, -block_size, SEEK_CUR);
     }
 
     fclose(input);
