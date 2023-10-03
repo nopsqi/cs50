53ccf3872083bea1472367198a0f0eba6a672bcf
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-03 09:50:11 +0000

    Tue, Oct 3, 2023, 4:50 PM +07:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
     {
         if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) && (buffer[3] >= 0xe0 && buffer[3] <= 0xef))
         {
+            if 
             char outname[8];
             sprintf(outname, "%03d.jpg", image_counter);
             FILE *outfile = fopen(outname, "w");
