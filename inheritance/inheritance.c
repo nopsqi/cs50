458606f95649e3bfd3b938b643df5d80adc6bfbd
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-05 16:29:17 +0000

    Thu, Oct 5, 2023, 11:29 PM +07:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,8 +50,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        p->parent[0] = parent0
+        p->parent[1] = parent1
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        p->alleles[0] = parent[0]->alleles[rand() % 2]
+        p->alleles[1] = parent[1]->alleles[rand() % 2]
 
     }
 
@@ -68,7 +72,7 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
