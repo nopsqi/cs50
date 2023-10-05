aadc9d906fe53663f64c1296b4b8727662e03e44
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-05 16:44:36 +0000

    Thu, Oct 5, 2023, 11:44 PM +07:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -80,13 +80,15 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p->parents[0] == NULL && p->parents[1] == NULL)
-        free(p)
+    if (p == NULL)
+        return;
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
-
+    free(p);
 }
 
 // Print each family member and their alleles.
