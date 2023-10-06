9ed82cccab48933c453eee46ef7d11e025d4edbb
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-06 15:41:36 +0000

    Fri, Oct 6, 2023, 10:41 PM +07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -49,8 +49,8 @@ unsigned int hash(const char *word)
         int curr = toupper(word[i]) - 'A' + 1;
         int prev = i == 0 ? curr : toupper(word[i - 1]) - 'A' + 1;
         int next = word[i + 1] == '\0' ? curr : toupper(word[i + i]) - 'A' + 1;
-        hash += ( curr * abs(curr * (i) - prev * (i - 1)) * abs(curr * (i) - next * (i + 1)) );
-        // hash += (toupper(c) - 'A' + 1) * (i + 1);
+        // hash += ( curr * abs(curr * (i) - prev * (i - 1)) * abs(curr * (i) - next * (i + 1)) );
+        hash += (toupper(c) - 'A' + 1) * (i + 1);
         // hash += pow((toupper(c) - 'A' + 1) * (i + 1), 2);
         // hash += pow((toupper(c) - 'A' + 1), (i + 1));
         printf("%c ", c);
