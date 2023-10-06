f3ae1f6944fac97017eedcd8ba2a19714498e254
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-06 23:05:52 +0000

    Sat, Oct 7, 2023, 6:05 AM +07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -20,7 +20,7 @@ node;
 // TODO: Choose number of buckets in hash table
 // const unsigned int N = 26;
 // N = Σ_(i = 0)^(LENGTH) ('z' - 'A' + 1) * (i + 1) ≈ 60000;
-const unsigned int N = 1000;
+const unsigned int N = 100000;
 
 // Hash table
 node *table[N] = {NULL};
@@ -40,7 +40,6 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     int hash = 0;
-    printf("\n");
     // for (int i = 0, c = word[i]; c != '\0'; c = word[++i])
     for (int i = 0; word[i] != '\0'; i++)
     {
@@ -53,7 +52,7 @@ unsigned int hash(const char *word)
         // hash += pow((toupper(c) - 'A' + 1) * (i + 1), 2);
         // hash += pow(toupper(c) - 'A' + 1, i + 1);
         // printf("%c,", c);
-        if (i > 1)
+        if (i > 3)
             break;
     }
     return abs(hash - 1);
@@ -100,7 +99,6 @@ bool create_hash_table(void)
                 return false;
             }
             strcpy(ptr->word, word);
-            printf("linked list created %s %u\n", word, hashes_word);
             if (table[hashes_word] == NULL)
             {
                 ptr->next = NULL;
@@ -110,6 +108,7 @@ bool create_hash_table(void)
             {
                 ptr->next = table[hashes_word];
                 table[hashes_word] = ptr;
+                printf("linked list created %s %u\n", word, hashes_word);
             }
 
             // Prepare for next word
