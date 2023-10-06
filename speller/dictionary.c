7b6066d5ca9c51b4dabf02a967a9f0def440cf2a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-06 10:18:37 +0000

    Fri, Oct 6, 2023, 5:18 PM +07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -40,13 +40,9 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     unsigned int hash = 0;
-    // for (int i = 0, c = word[i]; c != '\0'; c = word[++i])
-    for (int i = 1, c = word[i]; c != '\0'; c = word[++i])
+    for (int i = 0, c = word[i]; c != '\0'; c = word[++i])
     {
-        // hash += (toupper(c) - 'A' + 1) * (i + 1);
-        unsigned int curr = toupper(word[i]) - 'A' + 1;
-        unsigned int prev = toupper(word[i - 1]) - 'A' + 1;
-        hash += pow((curr - prev), 2);
+        hash += (toupper(c) - 'A' + 1) * (i + 1);
     }
     return hash;
 }
