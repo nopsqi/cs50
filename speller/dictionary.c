09360dd14f3ce6a65500d2a621c12c96583581ca
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-06 09:38:44 +0000

    Fri, Oct 6, 2023, 4:38 PM +07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -42,7 +42,7 @@ unsigned int hash(const char *word)
     unsigned int hash = 0;
     for (int i = 0, c = word[i]; c != '\0'; c = word[++i])
     {
-        hash += (toupper(c) - 'A' + 1);
+        hash += (toupper(c) - 'A' + 1) * (i + 1);
     }
     return hash;
 }
