cb840774b3f88c2b44b2795bd6f40dccac4c8216
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-07 08:48:50 +0000

    Sat, Oct 7, 2023, 3:48 PM +07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -55,18 +55,17 @@ unsigned int hash(const char *word)
         // }
         // h += tmp * (c / 100);
         // h += tmp;
-        // h += (c * (i + 1));
-        h += pow(c, i + 1);
-        // h += (c * pow(i + 1, 3));
+        h += (c * (i + 1));
+        // h += pow(c, i + 1);
+        // n += (c * pow(i + 1, 3));
         // h += toupper(c) - 'A' + 1;
         // h += pow((toupper(c) - 'A' + 1) * (i + 1), 2);
         // h += pow(c, i + 1);
         // printf("%c,", c);
-        n += (c * pow(i + 1, 3));
         if (i > 3)
             break;
     }
-    return labs(h) / n;
+    return labs(h);
 }
 
 bool create_hash_table(void)
