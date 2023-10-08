61843e6b621620f49c7cb2fe758755f33ac15271
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 10:27:20 +0000

    Sun, Oct 8, 2023, 5:27 PM +07:00

--- python/sentimental-mario-more/mario.py
+++ python/sentimental-mario-more/mario.py
@@ -8,8 +8,9 @@ def main():
 def get_int(prompt):
     height = 0
     while True:
-        if (height := int(input("Height: "))) in range(1, 9):
-            return height
+        try:
+            height = int(input("Height: "))
+        e
 
 
 
