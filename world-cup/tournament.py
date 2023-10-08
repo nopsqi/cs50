123ab909d570241cec461000965489645918bdbb
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 07:52:59 +0000

    Sun, Oct 8, 2023, 2:52 PM +07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     with open(sys.argv[1], 'r') as file:
-        reader = csv.reader(file)
+        reader = csv.DictReader(file)
         for row in reader:
             print(row)
 
