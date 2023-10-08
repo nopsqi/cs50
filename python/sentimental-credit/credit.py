3c172a977f1f287d5bc24f080bbd85beb601e2a5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 12:26:41 +0000

    Sun, Oct 8, 2023, 7:26 PM +07:00

--- python/sentimental-credit/credit.py
+++ python/sentimental-credit/credit.py
@@ -1,12 +1,26 @@
 # TODO
+import sys
+
+
 def main():
-    card_number = get_string("Number: ")
-    print(card_number)
+    # card_number = get_string("Number: ")
+    card_number = str(4062901840)
+    if (check_card(card_number) == 0):
+        print("INVALID")
+        sys.exit(1)
+    print("VALID")
+
 
 def check_card(card_number):
-    
-    for i, n in enumerate(card_number):
-        if
+    luhn = 0;
+    for i, n in enumerate(card_number[::-1]):
+        n = int(n)
+        if i % 2 == 1:
+            luhn += n * 2
+        else:
+            luhn += n
+    return luhn
+
 
 def get_string(prompt):
     while True:
@@ -16,7 +30,6 @@ def get_string(prompt):
             return card_number
         except ValueError:
             continue
-
 
 
 if __name__ == "__main__":
