4b1e66efe46fb9e54cd4d1e13f552e2fb7caabc6
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 08:10:21 +0000

    Sun, Oct 8, 2023, 3:10 PM +07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -58,6 +58,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    simulate_round(teams)
 
 
 if __name__ == "__main__":
