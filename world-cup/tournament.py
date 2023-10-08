0ec7d0bbec09fd7859c3a4183a0e64fc61059fc7
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 08:06:07 +0000

    Sun, Oct 8, 2023, 3:06 PM +07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -25,7 +25,8 @@ def main():
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N)
-        simulate_tournament(teams)
+        winner = simulate_tournament(teams)
+        counts[winner] = 
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
