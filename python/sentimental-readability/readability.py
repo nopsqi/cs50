759953026a63494fbdd39b078ce871b3d9235486
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 13:02:18 +0000

    Sun, Oct 8, 2023, 8:02 PM +07:00

--- python/sentimental-readability/readability.py
+++ python/sentimental-readability/readability.py
@@ -1,6 +1,24 @@
 # TODO
+import sys
+
+
 def main():
     text = input("Input text: ")
+    print(check_text(text))
+    sys.exit(0)
+
+
+def check_text(text):
+    grade = coleman(text)
+    if grade > 16:
+        return "Grade 16+"
+    if grade < 1:
+        return "Before Grade 1"
+    return f"Grade {grade}"
+
+
+def coleman(text):
+    sentences = 
 
 
 if __name__ = "__main__":
