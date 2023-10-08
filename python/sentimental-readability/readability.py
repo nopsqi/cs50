4016f498c26ddb24d1ff924ab937fdcf926b98ff
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-08 13:31:35 +0000

    Sun, Oct 8, 2023, 8:31 PM +07:00

--- python/sentimental-readability/readability.py
+++ python/sentimental-readability/readability.py
@@ -29,7 +29,7 @@ def coleman(text):
     l = letters / words * 100
     s = sentences / words * 100
     index = 0.0588 * l - 0.296 * s - 15.8
-    return round(index)
+    return ceil(index)
 
 
 if __name__ == "__main__":
