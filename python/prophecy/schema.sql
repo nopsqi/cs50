f6382be6ac32dc447b3ab541b1b7305ff0792a02
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-09 22:51:52 +0000

    Tue, Oct 10, 2023, 5:51 AM +07:00

---
+++ python/prophecy/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE students (
+    id INTEGER NOT NULL,
+    student_name TEXT NOT NULL
+    PRIMARY KEY(id)
+);
+CREATE TABLE houses (
+    id INTEGER NOT NULL,
+    house TEXT NOT NULL,
+    head, TEXT NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE assignment (
+    id INTEGER NOT NULL,
+    student_id 
+)
