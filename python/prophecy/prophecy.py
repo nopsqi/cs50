383ae89cedbc09b8078d2e97689ac3997dfdfc4b
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-09 23:16:57 +0000

    Tue, Oct 10, 2023, 6:16 AM +07:00

--- python/prophecy/prophecy.py
+++ python/prophecy/prophecy.py
@@ -8,7 +8,10 @@ def main():
     with open("students.csv") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            
+            row["id"] = int(row["id"])
+            data.append(row)
+    for d in data:
+        db.execute("INSERT INTO students (student_name) VALUES (?)", d["student_name"])
 
 
 if __name__ == "__main__":
