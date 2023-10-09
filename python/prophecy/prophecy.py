8e5464dee09cb2c0c9b19f9632fea7dea179dc85
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-09 23:48:50 +0000

    Tue, Oct 10, 2023, 6:48 AM +07:00

--- python/prophecy/prophecy.py
+++ python/prophecy/prophecy.py
@@ -15,11 +15,12 @@ def main():
         house = {k: v for k, v in d.items() if k not in ["student_name", "id"]}
         if house not in houses:
             houses.append(house)
-    for house in sorted(houses, key=lambda house: house["house"]):
-        db.execute("INSERT INTO houses (house, head) VALUES (?, ?)", house["house"], house["head"])
+    # for house in sorted(houses, key=lambda house: house["house"]):
+    #     db.execute("INSERT INTO houses (house, head) VALUES (?, ?)", house["house"], house["head"])
     for d in data:
-        pass
         # db.execute("INSERT INTO students (student_name) VALUES (?)", d["student_name"])
+        house_id = db.execute("SELECT id FROM houses WHERE house = ?", d["house"])[0]["id"]
+        db.execute("INSERT INTO assignment (student_id, house_id) VALUES (?, ?)", d["id"], house_id)
 
 
 if __name__ == "__main__":
