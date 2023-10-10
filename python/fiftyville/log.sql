e70192fd202100ea68a7b54dc8ff65ca5dd5a363
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 17:44:15 +0000

    Wed, Oct 11, 2023, 12:44 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -37,9 +37,7 @@ SELECT
     , pc.receiver
     , pc.duration
     , a.city destination
-    -- , p1.name
-    -- , p1.passport_number
-    -- , a1.city origin
+    , p1.name
 FROM crime_scene_reports c
 JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.day = c.day AND atm.atm_location = 'Leggett Street' AND atm.transaction_type = 'withdraw'
 JOIN bank_accounts ba ON ba.account_number = atm.account_number
@@ -48,7 +46,8 @@ JOIN people p ON p.id = ba.person_id AND p.license_plate = b.license_plate
 JOIN phone_calls pc ON (pc.year = c.year AND pc.month = c.month AND pc.day = c.day) AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number) AND pc.duration < 60
 JOIN passengers pas ON pas.passport_number = p.passport_number
 JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
-JOIN airports a ON a.id = f.destination_airport_id
+JOIN airports a ON a.id = f.destination_airport_id and a.city = 'Fiftyville'
+JOIN people p1 ON p1.phone_number = pc.receiver
 -- JOIN people p1 ON p1.phone_number = pc.receiver
 -- JOIN airports a ON a.id = f.origin_airport_id
 WHERE c.year = 2021
