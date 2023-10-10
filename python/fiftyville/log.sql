bb4ff05807c9732ed1e783347a4dd026e3694274
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 17:09:36 +0000

    Wed, Oct 11, 2023, 12:09 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -4,7 +4,13 @@
 
 SELECT i.name, i.transcript
 FROM interviews
-WHERE
+WHERE c.year = 2021
+AND c.month = 7
+AND c.day = 28
+AND c.street = 'Humphrey Street'
+AND c.description LIKE '%theft%'
+AND i.transcript LIKE '%bakery%'
+;
 
 SELECT
     i.year, i.month, i.day
