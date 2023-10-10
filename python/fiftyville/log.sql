c80ad21797211d43ba65954fe8997e7cb638f9a0
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 23:02:18 +0000

    Wed, Oct 11, 2023, 6:02 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -25,22 +25,22 @@ SELECT
     -- , atm.year
     -- , atm.month
     -- , atm.day
-    , atm.account_number
-    , atm.atm_location
-    , atm.transaction_type
-    , atm.amount
-    , p.name
-    , p.phone_number
+    -- , atm.account_number
+    -- , atm.atm_location
+    -- , atm.transaction_type
+    -- , atm.amount
+    -- , p.name
+    -- , p.phone_number
     -- , p.passport_number
     , pc.caller
     , pc.receiver
     , pc.duration
     , p1.name
     , p1.passport_number
+    , a.city destination
     -- , b.hour || ':' || b.minute as time
     -- , b.activity
     -- , b.license_plate
-    -- , a.city destination
     -- , p1.name
 FROM crime_scene_reports c
 JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.day = c.day AND atm.atm_location = 'Leggett Street' AND atm.transaction_type = 'withdraw'
@@ -50,7 +50,7 @@ JOIN phone_calls pc ON pc.year = c.year AND pc.month = c.month AND pc.day = c.da
 JOIN people p1 ON p1.phone_number = pc.receiver
 JOIN passengers pas ON pas.passport_number = p1.passport_number
 JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day -- AND f.hour >= b.hour AND f.minute >= b.minute
--- JOIN airports a ON a.id = f.destination_airport_id -- AND a.city = 'Fiftyville'
+JOIN airports a ON a.id = f.destination_airport_id -- AND a.city = 'Fiftyville'
 -- JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day AND b.hour >= 10 AND b.minute >= 15 -- AND b.activity = 'exit'
 -- JOIN flights f1 ON f1.id = pas.flight_id AND f1.year >= f.year AND f.month >=c.month AND  f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
 -- JOIN airports a1 ON a1.id = f1.destination_airport_id
