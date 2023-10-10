4d15199c7904f9aa40d0b31d8d123098b8d52f99
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 01:56:11 +0000

    Tue, Oct 10, 2023, 8:56 AM +07:00

--- python/movies/12.sql
+++ python/movies/12.sql
@@ -1,1 +1,12 @@
-SELECT movies.title 
+SELECT movies.title FROM movies
+JOIN people, stars ON people.id = stars.person_id
+WHERE people.id =
+(
+    SELECT people.id FROM people
+    WHERE people.name = 'Bradley Cooper'
+)
+OR people.id =
+(
+    SELECT people.id FROM people
+    WHERE people.name = 'Jennifer Lawrence'
+);
