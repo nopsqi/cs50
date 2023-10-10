f843f8c93b27c0311debaba5bcc4d7285dbabe35
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 09:52:42 +0000

    Tue, Oct 10, 2023, 4:52 PM +07:00

--- python/movies/playground.sql
+++ python/movies/playground.sql
@@ -1,7 +1,6 @@
 SELECT *
 FROM people p1
-JOIN stars s1 ON s1.person_id = p1.id
-JOIN movies m ON m.id = s1.movie_id
-JOIN stars s2 ON s2.movie_id = m.id
-WHERE 
+INNER JOIN stars s1 ON s1.person_id = p1.id
+INNER JOIN movies m ON m.id = s1.movie_id
+INNER JOIN stars s2 ON s2.movie_id = m.id
 LIMIT 20;
