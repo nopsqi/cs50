698d8dd72271614998a86f405400faab98853f2d
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 22:27:34 +0000

    Wed, Oct 11, 2023, 5:27 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -23,34 +23,33 @@ ORDER BY i.name
 SELECT
     c.year, c.month, c.day
     , atm.account_number
-    -- , atm.atm_location
-    -- , atm.transaction_type
-    -- , atm.amount
-    , p.name
-    , p.phone_number
-    , p.passport_number
-    , b.hour
-    , b.minute
-    , b.activity
-    , b.license_plate
-    , pc.caller
-    , pc.receiver
-    , pc.duration
-    , a.city destination
-    , p1.name
+    , atm.atm_location
+    , atm.transaction_type
+    , atm.amount
+    -- , p.name
+    -- , p.phone_number
+    -- , p.passport_number
+    -- , b.hour
+    -- , b.minute
+    -- , b.activity
+    -- , b.license_plate
+    -- , pc.caller
+    -- , pc.receiver
+    -- , pc.duration
+    -- , a.city destination
+    -- , p1.name
 FROM crime_scene_reports c
 JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.day = c.day AND atm.atm_location = 'Leggett Street' AND atm.transaction_type = 'withdraw'
-JOIN bank_accounts ba ON ba.account_number = atm.account_number
-JOIN people p ON p.id = ba.person_id AND p.license_plate = b.license_plate
-JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day AND b.hour >= 10 AND b.minute >= 15 AND b.activity = 'exit'
-JOIN phone_calls pc ON (pc.year = c.year AND pc.month = c.month AND pc.day = c.day) AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number) AND pc.duration < 60
-JOIN passengers pas ON pas.passport_number = p.passport_number
-JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
-JOIN airports a ON a.id = f.destination_airport_id -- AND a.city = 'Fiftyville'
-JOIN people p1 ON p1.phone_number = pc.receiver
+-- JOIN bank_accounts ba ON ba.account_number = atm.account_number
+-- JOIN people p ON p.id = ba.person_id AND p.license_plate = b.license_plate
+-- JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day AND b.hour >= 10 AND b.minute >= 15 AND b.activity = 'exit'
+-- JOIN phone_calls pc ON (pc.year = c.year AND pc.month = c.month AND pc.day = c.day) AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number) AND pc.duration < 60
+-- JOIN passengers pas ON pas.passport_number = p.passport_number
+-- JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
+-- JOIN airports a ON a.id = f.destination_airport_id -- AND a.city = 'Fiftyville'
+-- JOIN people p1 ON p1.phone_number = pc.receiver
 -- JOIN flights f1 ON f1.id = pas.flight_id AND f1.year >= f.year AND f.month >=c.month AND  f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
 -- JOIN airports a1 ON a1.id = f1.destination_airport_id
--- JOIN people p1 ON p1.phone_number = pc.receiver
 -- JOIN airports a ON a.id = f.origin_airport_id
 WHERE c.year = 2021
 AND c.month = 7
