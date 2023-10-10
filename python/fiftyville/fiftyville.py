b7fd270f7c93d11113bd896b8fa59453b0e69ab5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 13:05:30 +0000

    Tue, Oct 10, 2023, 8:05 PM +07:00

--- python/fiftyville/fiftyville.py
+++ python/fiftyville/fiftyville.py
@@ -6,6 +6,7 @@ def main():
     tables = db.execute("SELECT name FROM sqlite_master;")
     for table in tables:
         db.execute("SELECT * FROM ? LIMIT 5", table["name"])
+        break
 
 
 if __name__ == "__main__":
