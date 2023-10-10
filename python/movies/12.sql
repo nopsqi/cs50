053c8441d7dcde0a77a3157dd1f3afff898fa8aa
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 12:50:34 +0000

    Tue, Oct 10, 2023, 7:50 PM +07:00

--- python/movies/12.sql
+++ python/movies/12.sql
@@ -1,4 +1,4 @@
-SELECT *
+SELECT title
 FROM movies
 WHERE movies.id in (
     SELECT movie_id
@@ -7,6 +7,15 @@ WHERE movies.id in (
         SELECT id
         FROM people
         WHERE name = 'Bradley Cooper'
+    )
+)
+AND movies.id in (
+    SELECT movie_id
+    FROM stars
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = 'Jennifer Lawrence'
     )
 )
 LIMIT 20;
