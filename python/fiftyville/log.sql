936731f4bcfb7a311f49048d5eafd332b1de07bd
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 23:36:53 +0000

    Wed, Oct 11, 2023, 6:36 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -49,7 +49,7 @@ JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.d
 JOIN bank_accounts ba ON ba.account_number = atm.account_number
 JOIN people p ON p.id = ba.person_id AND p.license_plate = b.license_plate
 JOIN phone_calls pc ON pc.year = c.year AND pc.month = c.month AND pc.day = c.day AND pc.duration < 60 AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number)
-JOIN people p1 ON p1.phone_number = pc.receiver
+JOIN people p1 ON IF(p1.phone_number = pc.caller, )
 JOIN passengers pas ON pas.passport_number = p1.passport_number OR pas.passport_number = p.passport_number
 JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day = c.day + 1
 JOIN airports a ON a.id = f.destination_airport_id
