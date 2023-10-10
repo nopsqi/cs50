2483dc0e63e60d8b69a23f430579a36dd6702c7b
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 14:09:27 +0000

    Tue, Oct 10, 2023, 9:09 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -2,16 +2,16 @@
 
 .schema
 
-SELECT i.day, i.month, i.year, i.name, i.transcript
+SELECT
+    i.day, i.month, i.year, b.hour, b.minute
+    ,i.name, c.description, i.transcript
 FROM crime_scene_reports c
-JOIN interviews i
+JOIN interviews i, bakery_security_logs b
 WHERE c.year = 2021
 AND c.month = 7
 AND c.day = 28
 AND c.street = 'Humphrey Street'
 AND c.description LIKE '%theft%'
-AND i.transcript LIKE '%theft%';
-
-SELECT *
-FROM bakery_security_logs
-LIMIT 10;
+AND i.transcript LIKE '%theft%'
+AND b.hour = 10
+AND B
