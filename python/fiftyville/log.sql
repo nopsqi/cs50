1377f661272e3689b7e04e7e6e3a7241777ed004
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 22:57:34 +0000

    Wed, Oct 11, 2023, 5:57 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -35,6 +35,8 @@ SELECT
     , pc.caller
     , pc.receiver
     , pc.duration
+    , p1.name
+    , p1.passport_number
     -- , b.hour || ':' || b.minute as time
     -- , b.activity
     -- , b.license_plate
@@ -45,11 +47,11 @@ JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.d
 JOIN bank_accounts ba ON ba.account_number = atm.account_number
 JOIN people p ON p.id = ba.person_id -- AND p.license_plate = b.license_plate
 JOIN phone_calls pc ON pc.year = c.year AND pc.month = c.month AND pc.day = c.day AND pc.duration < 60 AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number)
--- JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day AND b.hour >= 10 AND b.minute >= 15 -- AND b.activity = 'exit'
--- JOIN passengers pas ON pas.passport_number = p.passport_number
--- JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
+JOIN people p1 ON p1.phone_number = pc.receiver
+JOIN passengers pas ON pas.passport_number = p1.passport_number
+JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day -- AND f.hour >= b.hour AND f.minute >= b.minute
 -- JOIN airports a ON a.id = f.destination_airport_id -- AND a.city = 'Fiftyville'
--- JOIN people p1 ON p1.phone_number = pc.receiver
+-- JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day AND b.hour >= 10 AND b.minute >= 15 -- AND b.activity = 'exit'
 -- JOIN flights f1 ON f1.id = pas.flight_id AND f1.year >= f.year AND f.month >=c.month AND  f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
 -- JOIN airports a1 ON a1.id = f1.destination_airport_id
 -- JOIN airports a ON a.id = f.origin_airport_id
