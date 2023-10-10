906995e5a083a6a97e104b6ed6dec53d1a5ebe3b
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 02:03:03 +0000

    Tue, Oct 10, 2023, 9:03 AM +07:00

--- python/movies/13.sql
+++ python/movies/13.sql
@@ -2,6 +2,9 @@ SELECT people.name FROM people
 JOIN movies, stars ON people.id = stars.person_id AND movies.id = stars.movie_id
 WHERE movies.id IN
 (
-    SELECT movies.id FROM movie
-    WHERE 
+    SELECT stars.movies_id FROM stars
+    WHERE stars.person_id =
+    (
+        
+    )
 )
