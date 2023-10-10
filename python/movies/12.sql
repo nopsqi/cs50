258ab309a84dc46d7193b11c26f6a21a6c7dc221
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 02:21:13 +0000

    Tue, Oct 10, 2023, 9:21 AM +07:00

--- python/movies/12.sql
+++ python/movies/12.sql
@@ -1,13 +1,14 @@
-SELECT movies.title FROM movies
+SELECT * FROM movies
 JOIN stars ON movies.id = stars.movie_id
-WHERE stars.person_id =
-(
-    SELECT people.id FROM people
-    WHERE people.name = 'Bradley Cooper'
-)
-AND stars.person_id =
-(
-    SELECT people.id FROM people
-    WHERE people.name = 'Jennifer Lawrence'
-)
-GROUP BY movies.title;
+-- WHERE stars.person_id =
+-- (
+--     SELECT people.id FROM people
+--     WHERE people.name = 'Bradley Cooper'
+-- )
+-- AND stars.person_id =
+-- (
+--     SELECT people.id FROM people
+--     where people.name = 'jennifer lawrence'
+-- )
+GROUP BY movies.title
+LIMIT 10;
