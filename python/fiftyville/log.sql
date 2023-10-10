a44b53a57945e61f3e12e75bd0b8fb72766ecad6
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 15:48:48 +0000

    Tue, Oct 10, 2023, 10:48 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -4,12 +4,16 @@
 
 SELECT
     i.year, i.month, i.day, b.hour, b.minute
-    -- , i.name, c.description, i.transcript
-    -- , b.activity, b.license_plate
-    -- , p.id, p.name, p.phone_number, p.passport_number
-    -- , pc.caller, pc.receiver
-    -- , p1.name, p1.phone_number, p1.passport_number
-    , f.
+    , c.description
+    , i.transcript
+    , b.activity
+    , b.license_plate
+    , p.name
+    , pc.caller
+    , pc.receiver
+    , p1.name
+    , p1.passport_number
+    , a.city destination
 FROM crime_scene_reports c
 JOIN interviews i, bakery_security_logs b
 JOIN people p ON p.license_plate = b.license_plate
@@ -17,6 +21,7 @@ JOIN phone_calls pc ON pc.caller = p.phone_number AND pc.day = c.day
 JOIN people p1 ON p1.phone_number = pc.receiver
 JOIN passengers pas ON pas.passport_number = p1.passport_number
 JOIN flights f ON f.id = pas.flight_id
+JOIN airports a ON a.id = f.destination_airport_id
 WHERE c.year = 2021
 AND c.month = 7
 AND c.day = 28
