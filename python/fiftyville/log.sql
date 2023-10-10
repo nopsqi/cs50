e777bf2d3d4216cfbefe8ac50b7cf2eb495cd710
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 22:19:45 +0000

    Wed, Oct 11, 2023, 5:19 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -46,9 +46,10 @@ JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day =
 JOIN phone_calls pc ON (pc.year = c.year AND pc.month = c.month AND pc.day = c.day) AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number) AND pc.duration < 60
 JOIN passengers pas ON pas.passport_number = p.passport_number
 JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
-JOIN airports a ON a.id = f.destination_airport_id and a.city = 'Fiftyville'
+JOIN airports a ON a.id = f.destination_airport_id -- AND a.city = 'Fiftyville'
 JOIN people p1 ON p1.phone_number = pc.receiver
-JOIN flight f1 ON f1.id = pas.flight_id AND f1.year >= f.year AND f.month >=c.month AND  f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
+-- JOIN flights f1 ON f1.id = pas.flight_id AND f1.year >= f.year AND f.month >=c.month AND  f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
+-- JOIN airports a1 ON a1.id = f1.destination_airport_id
 -- JOIN people p1 ON p1.phone_number = pc.receiver
 -- JOIN airports a ON a.id = f.origin_airport_id
 WHERE c.year = 2021
