7c942003b5665fa1ffd4ec40e7bfdb5960fd8bf4
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 15:31:30 +0000

    Tue, Oct 10, 2023, 10:31 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -7,13 +7,16 @@ SELECT
     -- , i.name, c.description, i.transcript
     -- , b.activity, b.license_plate
     -- , p.name, p.phone_number, p.passport_number
+    , a.account_number, a.year. a.month, a.day, a.transaction_type, a.amount
     -- , pc.caller, pc.receiver
-    , p1.name, p1.phone_number, p1.passport_number
+    -- , p1.name, p1.phone_number, p1.passport_number
 FROM crime_scene_reports c
 JOIN interviews i, bakery_security_logs b
 JOIN people p ON p.license_plate = b.license_plate
 JOIN phone_calls pc ON pc.caller = p.phone_number AND pc.day = c.day
 JOIN people p1 ON p1.phone_number = pc.receiver
+JOIN bank_account ba ON ba.person_id = p.id
+JOIN atm_transactions a ON a.account_number = ba.account_number
 WHERE c.year = 2021
 AND c.month = 7
 AND c.day = 28
