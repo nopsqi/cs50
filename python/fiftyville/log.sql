d67edf453bc366117670f6fe451cb995cc4351aa
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 15:57:15 +0000

    Tue, Oct 10, 2023, 10:57 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -3,33 +3,35 @@
 .schema
 
 SELECT
-    i.year, i.month, i.day, b.hour, b.minute
-    , c.description
+    i.year, i.month, i.day
+    -- , c.description
     , i.transcript
-    , b.minute
-    , b.activity
-    , b.license_plate
-    , p.name
-    , pc.caller
-    , pc.receiver
-    , p1.name
-    , p1.passport_number
-    , a.city origin
-    , a1.city destination
+    -- , b.hour
+    -- , b.minute
+    -- , b.activity
+    -- , b.license_plate
+    -- , p.name
+    -- , pc.caller
+    -- , pc.receiver
+    -- , p1.name
+    -- , p1.passport_number
+    -- , a.city origin
+    -- , a1.city destination
 FROM crime_scene_reports c
-JOIN interviews i, bakery_security_logs b
-JOIN people p ON p.license_plate = b.license_plate
-JOIN phone_calls pc ON pc.caller = p.phone_number AND pc.day = c.day
-JOIN people p1 ON p1.phone_number = pc.receiver
-JOIN passengers pas ON pas.passport_number = p1.passport_number
-JOIN flights f ON f.id = pas.flight_id
-JOIN airports a ON a.id = f.origin_airport_id
-JOIN airports a1 ON a1.id = f.destination_airport_id
+JOIN interviews i
+-- JOIN bakery_security_logs b
+-- JOIN people p ON p.license_plate = b.license_plate
+-- JOIN phone_calls pc ON pc.caller = p.phone_number AND pc.day = c.day
+-- JOIN people p1 ON p1.phone_number = pc.receiver
+-- JOIN passengers pas ON pas.passport_number = p1.passport_number
+-- JOIN flights f ON f.id = pas.flight_id
+-- JOIN airports a ON a.id = f.origin_airport_id
+-- JOIN airports a1 ON a.id = f.destination_airport_id
 WHERE c.year = 2021
 AND c.month = 7
 AND c.day = 28
 AND c.street = 'Humphrey Street'
 AND c.description LIKE '%theft%'
-AND i.transcript LIKE '%theft%'
-AND b.hour = 10
-AND b.minute = 25;
+AND i.transcript LIKE '%bakery%'
+-- AND b.hour = 10
+-- AND b.minute = 25;
