64ef176d9acb944a7353a2192cc9105b5a003daf
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 02:08:17 +0000

    Tue, Oct 10, 2023, 9:08 AM +07:00

--- python/movies/13.sql
+++ python/movies/13.sql
@@ -2,10 +2,11 @@ SELECT people.name FROM people
 JOIN stars ON people.id = stars.person_id
 WHERE stars.movie_id IN
 (
-    SELECT stars.movies_id FROM stars
+    SELECT stars.movie_id FROM stars
     WHERE stars.person_id =
     (
         SELECT people.id FROM people
-        WHERE people.name = 'Kevin Bacon'
+        WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
     )
-);
+)
+AND people.name != 'Kevin Bacon';
