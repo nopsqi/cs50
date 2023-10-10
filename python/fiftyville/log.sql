bca1632c39696abc8faa8c87536199be7f399870
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 17:40:37 +0000

    Wed, Oct 11, 2023, 12:40 AM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -41,13 +41,13 @@ SELECT
     -- , p1.passport_number
     -- , a1.city origin
 FROM crime_scene_reports c
-JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.day = c.day AND atm.atm_location = 'Leggett Street'
+JOIN atm_transactions atm ON atm.year = c.year AND atm.month = c.month AND atm.day = c.day AND atm.atm_location = 'Leggett Street' AND atm.transaction_type = 'withdraw'
 JOIN bank_accounts ba ON ba.account_number = atm.account_number
-JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day
+JOIN bakery_security_logs b ON b.year = c.year AND b.month = c.month AND b.day = c.day AND b.hour >= 10 AND b.minute >= 15 AND b.activity = 'exit'
 JOIN people p ON p.id = ba.person_id AND p.license_plate = b.license_plate
-JOIN phone_calls pc ON (pc.year = c.year AND pc.month = c.month AND pc.day = c.day) AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number)
+JOIN phone_calls pc ON (pc.year = c.year AND pc.month = c.month AND pc.day = c.day) AND (pc.caller = p.phone_number OR pc.receiver = p.phone_number) AND pc.duration < 60
 JOIN passengers pas ON pas.passport_number = p.passport_number
-JOIN flights f ON f.id = pas.flight_id
+JOIN flights f ON f.id = pas.flight_id AND f.year >= c.year AND f.month >= c.month AND f.day >= c.day AND f.hour >= b.hour AND f.minute >= b.minute
 JOIN airports a ON a.id = f.destination_airport_id
 -- JOIN people p1 ON p1.phone_number = pc.receiver
 -- JOIN airports a ON a.id = f.origin_airport_id
@@ -56,10 +56,5 @@ AND c.month = 7
 AND c.day = 28
 AND c.street = 'Humphrey Street'
 AND c.description LIKE '%theft%'
-AND atm.transaction_type = 'withdraw'
-AND b.hour >= 10
-AND b.minute >= 15
-AND b.activity = 'exit'
-AND pc.duration < 60
 -- GROUP BY i.transcript
 ;
