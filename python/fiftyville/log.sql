712a2e88dd59da3d934a2490f7408a312cdad153
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 15:40:55 +0000

    Tue, Oct 10, 2023, 10:40 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -3,12 +3,10 @@
 .schema
 
 SELECT
-    i.day, i.month, i.year, b.hour, b.minute
+    i.year, i.month, i.day, b.hour, b.minute
     -- , i.name, c.description, i.transcript
     -- , b.activity, b.license_plate
-    , p.id, p.name, p.phone_number, p.passport_number
-    -- , ba.account_number
-    , a.account_number, a.year, a.month, a.day, a.transaction_type, a.amount
+    -- , p.id, p.name, p.phone_number, p.passport_number
     -- , pc.caller, pc.receiver
     -- , p1.name, p1.phone_number, p1.passport_number
 FROM crime_scene_reports c
@@ -16,8 +14,7 @@ JOIN interviews i, bakery_security_logs b
 JOIN people p ON p.license_plate = b.license_plate
 JOIN phone_calls pc ON pc.caller = p.phone_number AND pc.day = c.day
 JOIN people p1 ON p1.phone_number = pc.receiver
-JOIN bank_accounts ba ON ba.person_id = p1.id
-JOIN atm_transactions a ON a.account_number = ba.account_number
+JOIN passengers pas ON pas.passport_number = p1.passport_number
 WHERE c.year = 2021
 AND c.month = 7
 AND c.day = 28
