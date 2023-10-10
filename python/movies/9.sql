e1df1496ed2d7c04e88e93ca3529e0e2761ff8b6
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 01:29:02 +0000

    Tue, Oct 10, 2023, 8:29 AM +07:00

--- python/movies/9.sql
+++ python/movies/9.sql
@@ -1,1 +1,5 @@
 SELECT people.name FROM people
+JOIN movies, stars ON movies.id = stars.movie_id AND people.id = stars.person_id
+WHERE movies.year = 2004
+GROUP BY people.name
+ORDER BY people.birth;
