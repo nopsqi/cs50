20a21b38fd9d5aaa8127c9724daede9701d97897
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 13:36:54 +0000

    Tue, Oct 10, 2023, 8:36 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -2,7 +2,7 @@
 
 .table
 
-SELECT i
+SELECT i.name, i.transcript
 FROM crime_scene_reports c
 JOIN interviews i
 WHERE c.year = 2021 AND c.month = 7 AND c.day = 28 AND c.street = 'Humphrey Street' AND c.description LIKE '%theft%';
