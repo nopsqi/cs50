4fc9a8674becb54dccb5dcda2b45aa06bfdf3f09
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 13:04:47 +0000

    Tue, Oct 10, 2023, 8:04 PM +07:00

--- python/fiftyville/fiftyville.py
+++ python/fiftyville/fiftyville.py
@@ -1,8 +1,11 @@
-from CS50 import SQL
+from cs50 import SQL
 
 
 def main():
-    
+    db = SQL("sqlite:///fiftyville.db")
+    tables = db.execute("SELECT name FROM sqlite_master;")
+    for table in tables:
+        db.execute("SELECT * FROM ? LIMIT 5", table["name"])
 
 
 if __name__ == "__main__":
