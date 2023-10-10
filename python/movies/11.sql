33251443e4b2b3c8f0b447bb9d230445b6fcf802
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 02:17:04 +0000

    Tue, Oct 10, 2023, 9:17 AM +07:00

--- python/movies/11.sql
+++ python/movies/11.sql
@@ -1,9 +1,8 @@
 SELECT movies.title FROM movies
-JOIN stars, ratings, people ON movies.id = stars.movie_id AND people.id = stars.person_id
+JOIN stars, ratings ON movies.id = stars.movie_id AND movies.id = ratings.movie_id
 WHERE stars.person_id = (
     SELECT people.id FROM people
     WHERE people.name = 'Chadwick Boseman'
 )
-GROUP BY movies.title
 ORDER BY ratings.rating DESC
 LIMIT 5;
