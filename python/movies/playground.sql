48e5481a39f01b90018f5aab88f0c8d1e8c2153a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 11:40:42 +0000

    Tue, Oct 10, 2023, 6:40 PM +07:00

--- python/movies/playground.sql
+++ python/movies/playground.sql
@@ -1,8 +1,8 @@
-SELECT m.title, p2.name
+SELECT *
 FROM people p1
 INNER JOIN stars s1 ON s1.person_id = p1.id
 INNER JOIN movies m ON m.id = s1.movie_id
 INNER JOIN stars s2 ON s2.movie_id = m.id
 INNER JOIN people p2 ON p2.id = s2.person_id
-WHERE p2.name == 'Kevin Bacon'
+-- WHERE p1.name == 'Kevin Bacon'
 LIMIT 20;
