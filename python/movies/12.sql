c3b0daf2076686fe0ee01d5e1e45bd0cd1adc5a5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 12:47:54 +0000

    Tue, Oct 10, 2023, 7:47 PM +07:00

--- python/movies/12.sql
+++ python/movies/12.sql
@@ -1,5 +1,12 @@
 SELECT *
-FROM (
-    SELECT *
-    FROM 
+FROM movies
+WHERE movies.id in (
+    SELECT movie_id
+    FROM stars
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = 'Bradley Cooper'
+    )
 )
+LIMIT 20;
