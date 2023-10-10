7bcc047f2739bf411096d6230c5088dcc801704f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 08:58:53 +0000

    Tue, Oct 10, 2023, 3:58 PM +07:00

--- python/movies/playground.sql
+++ python/movies/playground.sql
@@ -1,8 +1,3 @@
--- SELECT DISTINCT p.name
 SELECT *
-FROM stars s1
-JOIN stars s2 ON s1.movie_id = s2.movie_id
-JOIN people p ON s2.person_id = p.id
--- WHERE s1.person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958)
---   AND p.name != 'Kevin Bacon';
-LIMIT 20;
+FROM people p1
+JOIN stars
