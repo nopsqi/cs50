cda2fa6bdd1c001bd10509966303f8319b866186
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 15:34:31 +0000

    Tue, Oct 10, 2023, 10:34 PM +07:00

--- python/fiftyville/log.sql
+++ python/fiftyville/log.sql
@@ -6,8 +6,9 @@ SELECT
     i.day, i.month, i.year, b.hour, b.minute
     -- , i.name, c.description, i.transcript
     -- , b.activity, b.license_plate
-    -- , p.name, p.phone_number, p.passport_number
-    , a.account_number, a.year. a.month, a.day, a.transaction_type, a.amount
+    , p.id, p.name, p.phone_number, p.passport_number
+    -- , ba.account_number
+    -- , a.account_number, a.year, a.month, a.day, a.transaction_type, a.amount
     -- , pc.caller, pc.receiver
     -- , p1.name, p1.phone_number, p1.passport_number
 FROM crime_scene_reports c
@@ -15,8 +16,8 @@ JOIN interviews i, bakery_security_logs b
 JOIN people p ON p.license_plate = b.license_plate
 JOIN phone_calls pc ON pc.caller = p.phone_number AND pc.day = c.day
 JOIN people p1 ON p1.phone_number = pc.receiver
-JOIN bank_account ba ON ba.person_id = p.id
-JOIN atm_transactions a ON a.account_number = ba.account_number
+-- JOIN bank_accounts ba ON ba.person_id = p.id
+-- JOIN atm_transactions a ON a.account_number = ba.account_number
 WHERE c.year = 2021
 AND c.month = 7
 AND c.day = 28
