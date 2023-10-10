f16cf104864ccb1dcb20e00e68be2c8b77c48241
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-10 12:32:52 +0000

    Tue, Oct 10, 2023, 7:32 PM +07:00

--- python/movies/12.sql
+++ python/movies/12.sql
@@ -1,12 +1,5 @@
-SELECT * FROM movies
-JOIN stars ON movies.id = stars.movie_id
-WHERE stars.movies_id =
+SELECT *
+FROM
 (
-    SELECT stars.movie_id FROM stars
-    WHERE stars.person_id =
-);
--- AND stars.person_id =
--- (
---     SELECT people.id FROM people
---     where people.name = 'jennifer lawrence'
--- )
+    SELECT 
+)
