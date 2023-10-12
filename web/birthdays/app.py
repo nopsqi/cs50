b16c4a5f267ac89a8c9e19dc8929c387f93fb320
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-12 22:20:25 +0000

    Fri, Oct 13, 2023, 5:20 AM +07:00

--- web/birthdays/app.py
+++ web/birthdays/app.py
@@ -27,7 +27,8 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-
+        data = request.form
+        print(data)
         return redirect("/")
 
     else:
