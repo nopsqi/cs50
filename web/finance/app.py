9a44dc92a19fd156fa6f5de5e85d11070d2d5ac5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-13 06:44:38 +0000

    Fri, Oct 13, 2023, 1:44 PM +07:00

--- web/finance/app.py
+++ web/finance/app.py
@@ -113,15 +113,8 @@ def register():
         username = request.form.get("username")
         password = request.form.get("username")
         confirmation = request.form.get("confirmation")
-        if not username and not password and not confirmation:
+        if not username or not password or not confirmation:
             return apology("all field empty.")
-        if not username:
-            return apology("username empty.")
-        if not password:
-            return apology("password empty.")
-        if not confirmation:
-            return apology("password confrimation empty.")
-        return redirect("/register")
     else:
         return render_template("register.html")
 
