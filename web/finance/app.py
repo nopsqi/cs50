ece5fab1ef2138815fcfbb96a9d069310e34d410
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-13 09:56:07 +0000

    Fri, Oct 13, 2023, 4:56 PM +07:00

--- web/finance/app.py
+++ web/finance/app.py
@@ -42,7 +42,10 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
