0f5cede11479ed43b41df2bd71509e495977a61c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-13 16:36:47 +0000

    Fri, Oct 13, 2023, 11:36 PM +07:00

--- web/finance/app.py
+++ web/finance/app.py
@@ -36,8 +36,13 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    cash = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])[0]["cash"]
-    datas = db.execute("SELECT s.symbol, s.name, p.shares FROM portofolios p JOIN symbols s ON s.id = p.symbol_id WHERE p.user_id = ?;", session["user_id"])
+    cash = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])[0][
+        "cash"
+    ]
+    datas = db.execute(
+        "SELECT s.symbol, s.name, p.shares FROM portofolios p JOIN symbols s ON s.id = p.symbol_id WHERE p.user_id = ?;",
+        session["user_id"],
+    )
     for data in datas:
         result = lookup(data["symbol"])
         if result is None:
@@ -46,7 +51,13 @@ def index():
             data["price"] = result["price"]
         data["total"] = data["shares"] * data["price"]
 
-    return render_template("index.html", cash=cash, datas=datas, total=sum(data["total"] for data in datas) + cash, usd=usd)
+    return render_template(
+        "index.html",
+        cash=cash,
+        datas=datas,
+        total=sum(data["total"] for data in datas) + cash,
+        usd=usd,
+    )
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -66,7 +77,9 @@ def buy():
         if not request.form.get("shares"):
             return apology("Enter amount of share.")
 
-        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0][
+            "cash"
+        ]
         if result["price"] * int(request.form.get("shares")) > cash:
             return apology("Not enough cash.")
         cash = cash - (result["price"] * int(request.form.get("shares")))
@@ -75,13 +88,38 @@ def buy():
 
         symbol_id = get_symbol_id(db, result["symbol"])
         if not symbol_id:
-            symbol_id = db.execute("INSERT INTO symbols (symbol, name) VALUES (?, ?)", result["symbol"], result["name"])
-        db.execute("INSERT INTO histories (user_id, transacted, symbol_id, price, shares) VALUES (?, ?, ?, ?, ?)", session["user_id"], datetime.datetime.now(), symbol_id, result["price"], int(request.form.get("shares")))
-        rows = db.execute("SELECT shares FROM portofolios WHERE user_id = ? AND symbol_id = ? LIMIT 1;", session["user_id"], symbol_id)
+            symbol_id = db.execute(
+                "INSERT INTO symbols (symbol, name) VALUES (?, ?)",
+                result["symbol"],
+                result["name"],
+            )
+        db.execute(
+            "INSERT INTO histories (user_id, transacted, symbol_id, price, shares) VALUES (?, ?, ?, ?, ?)",
+            session["user_id"],
+            datetime.datetime.now(),
+            symbol_id,
+            result["price"],
+            int(request.form.get("shares")),
+        )
+        rows = db.execute(
+            "SELECT shares FROM portofolios WHERE user_id = ? AND symbol_id = ? LIMIT 1;",
+            session["user_id"],
+            symbol_id,
+        )
         if len(rows) != 1:
-            db.execute("INSERT INTO portofolios (user_id, symbol_id, shares) VALUES (?, ?, ?);", session["user_id"], symbol_id, int(request.form.get("shares")))
+            db.execute(
+                "INSERT INTO portofolios (user_id, symbol_id, shares) VALUES (?, ?, ?);",
+                session["user_id"],
+                symbol_id,
+                int(request.form.get("shares")),
+            )
         else:
-            db.execute("UPDATE portofolios SET shares = ? WHERE user_id = ? AND symbol_id = ?;", rows[0]["shares"] + int(request.form.get("shares")), session["user_id"], symbol_id)
+            db.execute(
+                "UPDATE portofolios SET shares = ? WHERE user_id = ? AND symbol_id = ?;",
+                rows[0]["shares"] + int(request.form.get("shares")),
+                session["user_id"],
+                symbol_id,
+            )
 
         return redirect("/")
     else:
@@ -92,7 +130,10 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    datas = db.execute("SELECT h.transacted, h.price, h.shares, s.symbol FROM histories h JOIN symbols s ON s.id = h.symbol_id WHERE user_id = ?;", session["user_id"])
+    datas = db.execute(
+        "SELECT h.transacted, h.price, h.shares, s.symbol FROM histories h JOIN symbols s ON s.id = h.symbol_id WHERE user_id = ?;",
+        session["user_id"],
+    )
 
     return render_template("history.html", datas=datas, usd=usd)
 
@@ -106,7 +147,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -116,10 +156,14 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -157,7 +201,11 @@ def quote():
         result["price"] = usd(result["price"])
         print(get_symbol_id(db, result["symbol"]))
         if not get_symbol_id(db, result["symbol"]):
-            db.execute("INSERT INTO symbols (symbol, name) VALUES (?, ?)", result["symbol"], result["name"])
+            db.execute(
+                "INSERT INTO symbols (symbol, name) VALUES (?, ?)",
+                result["symbol"],
+                result["name"],
+            )
         return render_template("quoted.html", result=result)
     else:
         return render_template("quote.html")
@@ -172,7 +220,9 @@ def register():
         confirmation = request.form.get("confirmation")
         if not username or not password or not confirmation:
             return apology("Please fill all field.")
-        is_username_exist = db.execute("SELECT username FROM users WHERE username = ?;", username)
+        is_username_exist = db.execute(
+            "SELECT username FROM users WHERE username = ?;", username
+        )
         if is_username_exist:
             return apology("username exist.")
         if password != confirmation:
@@ -190,7 +240,10 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    rows = db.execute("SELECT p.shares, s.id, s.symbol, s.name FROM portofolios p JOIN symbols s ON s.id = p.symbol_id WHERE p.user_id = ? AND p.shares > 0 GROUP BY p.symbol_id;", session["user_id"])
+    rows = db.execute(
+        "SELECT p.shares, s.id, s.symbol, s.name FROM portofolios p JOIN symbols s ON s.id = p.symbol_id WHERE p.user_id = ? AND p.shares > 0 GROUP BY p.symbol_id;",
+        session["user_id"],
+    )
     if request.method == "POST":
         if not request.form.get("symbol") and not request.form.get("shares"):
             return apology("all field empty.")
@@ -199,21 +252,44 @@ def sell():
         if not request.form.get("shares"):
             return apology("Select how many stock you want to sell")
         if request.form.get("symbol").upper() not in [row["symbol"] for row in rows]:
-            return apology(f"you don't have {request.form.get('symbol')} in your portofolio")
-        if int(request.form.get("shares")) > [row["shares"] for row in rows if row["symbol"] == request.form.get("symbol")][0]:
+            return apology(
+                f"you don't have {request.form.get('symbol')} in your portofolio"
+            )
+        if (
+            int(request.form.get("shares"))
+            > [
+                row["shares"]
+                for row in rows
+                if row["symbol"] == request.form.get("symbol")
+            ][0]
+        ):
             return apology(f"You don't have enough {request.form.get('symbol')}")
 
         result = lookup(request.form.get("symbol"))
         if result is None:
             return apology("Failed fo fetch data.")
 
-        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0][
+            "cash"
+        ]
         cash = cash + (result["price"] * int(request.form.get("shares")))
         db.execute("UPDATE users SET cash = ? WHERE id = ?;", cash, session["user_id"])
 
         symbol_id = get_symbol_id(db, result["symbol"])
-        db.execute("INSERT INTO histories (user_id, transacted, symbol_id, price, shares) VALUES (?, ?, ?, ?, ?)", session["user_id"], datetime.datetime.now(), symbol_id, result["price"], -int(request.form.get("shares")))
-        db.execute("UPDATE portofolios SET shares = ? WHERE user_id = ? AND symbol_id = ?;", rows[0]["shares"] - int(request.form.get("shares")), session["user_id"], symbol_id)
+        db.execute(
+            "INSERT INTO histories (user_id, transacted, symbol_id, price, shares) VALUES (?, ?, ?, ?, ?)",
+            session["user_id"],
+            datetime.datetime.now(),
+            symbol_id,
+            result["price"],
+            -int(request.form.get("shares")),
+        )
+        db.execute(
+            "UPDATE portofolios SET shares = ? WHERE user_id = ? AND symbol_id = ?;",
+            rows[0]["shares"] - int(request.form.get("shares")),
+            session["user_id"],
+            symbol_id,
+        )
 
         return redirect("/sell")
     else:
