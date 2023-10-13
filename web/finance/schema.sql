81892d2167986bb84e95fc52a671b8be261bbb0c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-13 10:35:48 +0000

    Fri, Oct 13, 2023, 5:35 PM +07:00

---
+++ web/finance/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    cash NUMERIC NOT NULL DEFAULT 10000.00
+);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE UNIQUE INDEX username ON users (username);
+CREATE TABLE histories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER,
+    transacted DATETIME NOT NULL,
+    symbol TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    shares INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
