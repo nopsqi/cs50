201589b64bb8fb6691d26eff305e58e2b701b0ff
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-13 13:01:07 +0000

    Fri, Oct 13, 2023, 8:01 PM +07:00

--- web/finance/schema.sql
+++ web/finance/schema.sql
@@ -20,4 +20,13 @@ CREATE TABLE histories (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (symbol_id) REFERENCES symbols(id)
 );
-CREATE TABLE 
+CREATE TABLE portfolios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER,
+    transacted DATETIME NOT NULL,
+    symbol_id TEXT NOT NULL,
+    price NUMERIC NOT NULL,
+    shares INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (symbol_id) REFERENCES symbols(id)
+)
