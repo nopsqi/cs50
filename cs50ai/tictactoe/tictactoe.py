f24fa201705c0a0868d4ed513be6a53b9cb0c72e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-14 14:37:51 +0000

    Sat, Oct 14, 2023, 9:37 PM +07:00

--- cs50ai/tictactoe/tictactoe.py
+++ cs50ai/tictactoe/tictactoe.py
@@ -37,7 +37,7 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    return set((i, j) for i, row in enumerate(board) for j, col in enumerate(row))
+    return set((i, j) for i, row in enumerate(board) for j, cell in enumerate(row) if cell is None)
     return "action"
 
 
