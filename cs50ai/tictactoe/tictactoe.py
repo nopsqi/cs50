1acb46c85dc036a6d09f1e20b77f07d35894e07f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-14 15:42:30 +0000

    Sat, Oct 14, 2023, 10:42 PM +07:00

--- cs50ai/tictactoe/tictactoe.py
+++ cs50ai/tictactoe/tictactoe.py
@@ -71,8 +71,8 @@ def winner(board):
             print((i_prev, j_prev), (i, j))
             if i == i_prev + 1 and j == j_prev - 1:
                 diag_state[1] += cell
-            j_prev = j
-        i_prev = i
+                j_prev = j
+                i_prev = i
 
 
     return row_state, col_state, diag_state
