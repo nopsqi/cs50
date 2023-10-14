8b2e2cfdd5a81d0261a3fd4890753f46c5b44592
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-14 14:07:58 +0000

    Sat, Oct 14, 2023, 9:07 PM +07:00

---
+++ cs50ai/tictactoe/test.py
@@ -0,0 +1,12 @@
+import tictactoe as ttt
+from util import *
+
+
+def main():
+    board =  [[EMPTY, EMPTY, EMPTY],
+              [EMPTY, EMPTY, EMPTY],
+              [EMPTY, EMPTY, EMPTY]]
+
+
+if __name__ = "__main__":
+    main()
