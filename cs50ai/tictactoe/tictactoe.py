78baf7d87d81cb6c4559c4960d832363c047ce8a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-14 23:29:13 +0000

    Sun, Oct 15, 2023, 6:29 AM +07:00

--- cs50ai/tictactoe/tictactoe.py
+++ cs50ai/tictactoe/tictactoe.py
@@ -135,7 +135,7 @@ def calculate(board):
     values = [calculate(b) for b in [result(board, a) for a in actions(board)]]
 
     pl = player(board)
-    print(pl, calculate(board))
+    print(pl)
     for row in board:
         print(row)
     print()
