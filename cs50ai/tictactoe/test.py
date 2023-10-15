3b08afe8bf5573b833e276104094b9b2635ba678
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-15 09:07:16 +0000

    Sun, Oct 15, 2023, 4:07 PM +07:00

--- cs50ai/tictactoe/test.py
+++ cs50ai/tictactoe/test.py
@@ -21,7 +21,7 @@ def main():
     #          [X,EMPTY,EMPTY]]
     board = [[X, O, X],
              [X, O, EMPTY],
-             [O, EMPTY, EMPTY]]
+             [EMPTY, EMPTY, EMPTY]]
     # board = [[O, X, X],
     #          [X, X, O],
     #          [O, X, O]]
