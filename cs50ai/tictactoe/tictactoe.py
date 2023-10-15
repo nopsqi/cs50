2938c814f7e07b12515765fc7c8516c672c05a8e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-15 12:20:57 +0000

    Sun, Oct 15, 2023, 7:20 PM +07:00

--- cs50ai/tictactoe/tictactoe.py
+++ cs50ai/tictactoe/tictactoe.py
@@ -176,12 +176,11 @@ def ab_pruning(board):
             print(f"{padding}{row}")
         print()
 
-        if terminal(node.state) or node.level > 0:
-            continue
 
         for a, b in [(act, result(node.state, act)) for act in actions(node.state)]:
-            child = Node(state=b, parent=node, utility=None, level=node.level+1, action=a)
-            frontier.add(child)
+            if node.level < 1:
+                child = Node(state=b, parent=node, utility=None, level=node.level+1, action=a)
+                frontier.add(child)
 
 
 def calculate(board):
