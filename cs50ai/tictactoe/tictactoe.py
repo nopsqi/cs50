256a436adedb4997986b5a931c3ac933d8f1794f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-15 12:18:03 +0000

    Sun, Oct 15, 2023, 7:18 PM +07:00

--- cs50ai/tictactoe/tictactoe.py
+++ cs50ai/tictactoe/tictactoe.py
@@ -176,7 +176,7 @@ def ab_pruning(board):
             print(f"{padding}{row}")
         print()
 
-        if terminal(node.state) or node.level < 3:
+        if terminal(node.state) or node.level == 1:
             continue
 
         for a, b in [(act, result(node.state, act)) for act in actions(node.state)]:
