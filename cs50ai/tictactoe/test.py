2fd0a3fd441f71e61180a506843369ed339b653f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-15 10:34:38 +0000

    Sun, Oct 15, 2023, 5:34 PM +07:00

--- cs50ai/tictactoe/test.py
+++ cs50ai/tictactoe/test.py
@@ -1,4 +1,6 @@
 from tictactoe import *
+import string
+from itertools import product
 
 def main():
     empty = [[EMPTY, EMPTY, EMPTY],
@@ -25,7 +27,21 @@ def main():
     # board = [[O, X, X],
     #          [X, X, O],
     #          [O, X, O]]
-    ab_pruning(board)
+    # ab_pruning(board)
+    letters = generate_combiations()
+    print(next(letters))
+
+
+def generate_combinations():
+    letters = string.ascii_uppercase  # 'A' to 'Z'
+
+    for r in range(1, 4):  # Generate up to 3-letter combinations (adjust as needed)
+        for combination in product(letters, repeat=r):
+            yield ''.join(combination)
+
+# Example usage:
+for combination in generate_combinations():
+    print(combination)
 
 
 if __name__ == "__main__":
