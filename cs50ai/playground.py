a4916f8fa6f149c44771361e82b3d6d707b67de2
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-16 22:31:35 +0000

    Tue, Oct 17, 2023, 5:31 AM +07:00

--- cs50ai/playground.py
+++ cs50ai/playground.py
@@ -1,21 +1,7 @@
 import curses
+from curses import wrapper
 
 def main(stdscr):
-    # Initialize the curses window
-    curses.curs_set(0)  # Hide the cursor
     stdscr.clear()
-    stdscr.refresh()
 
-    # Create two windows within the main window
-    top_window = stdscr.subwin(5, curses.COLS, 0, 0)
-    bottom_window = stdscr.subwin(curses.LINES - 5, curses.COLS, 5, 0)
-
-    top_window.addstr(0, 0, "This is the top window", curses.A_BOLD)
-    bottom_window.addstr(0, 0, "This is the bottom window")
-
-    top_window.refresh()
-    bottom_window.refresh()
-
-    stdscr.getch()
-
-curses.wrapper(main)
+wrapper(main)
