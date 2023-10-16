b847ea87407c39cdfe7b58589e02616d79c210c7
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-16 21:47:20 +0000

    Tue, Oct 17, 2023, 4:47 AM +07:00

--- cs50ai/minesweeper/test.py
+++ cs50ai/minesweeper/test.py
@@ -7,13 +7,17 @@ def main():
     size = 3
     game = Minesweeper(height=size, width=size, mines=3)
     encoder = {letter: move for letter, move in zip(string.ascii_lowercase, itertools.product(range(size), repeat=2))}
+    status = None
     while True:
         game.print()
+        if status is not None:
+            print(status)
         position = encoder.get(input("Move: "))
         if position is None:
+            status = None
             continue
         count = game.nearby_mines(position)
-        print(position, count)
+        status = f"{position} {count}"
         # ai = MinesweeperAI()
         # ai.add_knowledge((3, 1), 3)
 
