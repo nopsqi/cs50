b0c35ce87da422f145bb89a20ebe9be95cb364d1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-16 23:05:39 +0000

    Tue, Oct 17, 2023, 6:05 AM +07:00

--- cs50ai/minesweeper/minesweeper.py
+++ cs50ai/minesweeper/minesweeper.py
@@ -190,7 +190,7 @@ class MinesweeperAI():
 
     def learn(self):
         for a, b in itertools.combinations(self.knowledge, 2):
-            print(a, b)
+            if 
 
 
     def add_knowledge(self, cell, count):
