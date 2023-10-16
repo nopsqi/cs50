e3cb16ef81ae7fcc7d4c90db56f447202be06ebd
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-16 21:15:42 +0000

    Tue, Oct 17, 2023, 4:15 AM +07:00

--- cs50ai/minesweeper/test.py
+++ cs50ai/minesweeper/test.py
@@ -1,7 +1,9 @@
-from minesweeper import MinesweeperAI
+from minesweeper import Minesweeper, MinesweeperAI
 
 
 def main():
+    game = Minesweeper()
+    print(game.mines)
     ai = MinesweeperAI()
     ai.add_knowledge((3, 1), 3)
 
