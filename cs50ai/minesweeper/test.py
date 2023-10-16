c2dfa77008927f5b62303436359c7b82c38fc126
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-16 21:22:15 +0000

    Tue, Oct 17, 2023, 4:22 AM +07:00

--- cs50ai/minesweeper/test.py
+++ cs50ai/minesweeper/test.py
@@ -4,7 +4,7 @@ import itertools
 
 def main():
     game = Minesweeper()
-    print(list(itertools.permutations(range(8), 2)))
+    print(list(itertools.product(range(8), 2)))
     ai = MinesweeperAI()
     ai.add_knowledge((3, 1), 3)
 
