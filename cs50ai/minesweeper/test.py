fc78ae74a73d4f9496a5665c6ef4e94a7dc689ef
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-16 21:29:20 +0000

    Tue, Oct 17, 2023, 4:29 AM +07:00

--- cs50ai/minesweeper/test.py
+++ cs50ai/minesweeper/test.py
@@ -6,12 +6,18 @@ def main():
     game = Minesweeper()
     mines = game.mines
     position = None
+    count = 0
     for p in itertools.product(range(8), repeat=2):
-        if p not in mines:
-            position = p
-            break
-    ai = MinesweeperAI()
-    ai.add_knowledge((3, 1), 3)
+        if p in mines:
+            continue
+        position = p
+        count = game.nearby_mines(position)
+        break
+    count = game.nearby_mines(position)
+    game.print()
+    print(count)
+    # ai = MinesweeperAI()
+    # ai.add_knowledge((3, 1), 3)
 
 
 if __name__ == "__main__":
