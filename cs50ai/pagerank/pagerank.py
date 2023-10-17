064a493dee16d98ba3165331424839fa23c9ebdb
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-17 21:51:19 +0000

    Wed, Oct 18, 2023, 4:51 AM +07:00

--- cs50ai/pagerank/pagerank.py
+++ cs50ai/pagerank/pagerank.py
@@ -57,10 +57,13 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    result = {}
     links = corpus.get(page)
-    if links != 0:
-        print(links)
+    if links is None:
+        probability = damping_factor / len(corpus)
+        return {p: probability for p in corpus}
+
+    result = {}
+    probability = damping_factor
 
     return result
 
