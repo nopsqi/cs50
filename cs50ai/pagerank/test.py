aad6c5974bbda4be2ab9bb9dbc7d4fd6f20bedee
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-17 23:00:58 +0000

    Wed, Oct 18, 2023, 6:00 AM +07:00

--- cs50ai/pagerank/test.py
+++ cs50ai/pagerank/test.py
@@ -3,7 +3,7 @@ from pagerank import *
 
 def main():
     corpus = {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}}
-    # corpus = crawl("corpus0")
+    corpus = crawl("corpus0")
     t_model = transition_model(corpus, "1.html", DAMPING)
     s_pagerank = sample_pagerank(corpus, DAMPING, 10000)
     print(s_pagerank)
