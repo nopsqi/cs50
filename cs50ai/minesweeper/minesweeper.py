c68d88088c9e4eb18e386cde0aee8737cc2c2c88
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-17 02:06:46 +0000

    Tue, Oct 17, 2023, 9:06 AM +07:00

--- cs50ai/minesweeper/minesweeper.py
+++ cs50ai/minesweeper/minesweeper.py
@@ -263,7 +263,7 @@ class MinesweeperAI():
 
         self.mark_safe(cell)
         knowledge_copy = None
-        for _ in range(5):
+        while self.knowledge != knowledge_copy:
             print("learning")
             knowledge_copy = self.knowledge.copy()
             self.learn()
