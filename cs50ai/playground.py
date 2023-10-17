83a3de729ac06732debd377203d7b131922e0d29
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-17 00:31:55 +0000

    Tue, Oct 17, 2023, 7:31 AM +07:00

--- cs50ai/playground.py
+++ cs50ai/playground.py
@@ -1,10 +1,7 @@
-import curses
-from curses import wrapper
-
-def main(stdscr):
-    stdscr.clear()
-    stdscr.addstr(0, 0, "hello, world!")
-    stdscr.refresh()
-    stdscr.getch()
-
-wrapper(main)
+original_list = [1, 2, 2, 3, 4, 4, 5]
+unique_list = []
+# [unique_list.append(x) for x in original_list if x not in unique_list]
+for i in original_list:
+    if i not in unique_list:
+        unique_list.append(i)
+print(unique_list)
