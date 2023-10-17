56045649d2ba7e9e8f5244b4dfb7b24e393fdcc0
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-17 22:16:54 +0000

    Wed, Oct 18, 2023, 5:16 AM +07:00

--- cs50ai/pagerank/pagerank.py
+++ cs50ai/pagerank/pagerank.py
@@ -82,10 +82,10 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    print(list(corpus))
+
+    page = random.choice(list(corpus))
+    t_model = transition_model(corpus, page, damping_factor)
     return 0
-    page = random.choice(corpus)
-    t_model = transation_model(corpus, page, damping_factor)
 
 
 def iterate_pagerank(corpus, damping_factor):
