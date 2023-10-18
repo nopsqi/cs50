6c1d9650e3b2e76fb2c3edeaf87a71d51b5da70d
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-18 11:34:30 +0000

    Wed, Oct 18, 2023, 6:34 PM +07:00

---
+++ cs50ai/heredity/test.py
@@ -0,0 +1,29 @@
+from heredity import *
+
+
+def main():
+    people = load_data("data/family0.csv")
+
+    # Keep track of gene and trait probabilities for each person
+    probabilities = {
+        person: {
+            "gene": {
+                2: 0,
+                1: 0,
+                0: 0
+            },
+            "trait": {
+                True: 0,
+                False: 0
+            }
+        }
+        for person in people
+    }
+    names = set(people)
+    p_set = powerset(names)
+    joint = joint_probability(people, {"Harry"}, {"James"}, {"Harry", "James"})
+    print(joint)
+
+
+if __name__ == "__main__":
+    main()
