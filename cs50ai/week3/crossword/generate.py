2f7a4a6e092bbbd9b7150ca6b9303b76ad2dba74
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-19 17:28:29 +0000

    Fri, Oct 20, 2023, 12:28 AM +07:00

--- cs50ai/week3/crossword/generate.py
+++ cs50ai/week3/crossword/generate.py
@@ -135,7 +135,9 @@ class CrosswordCreator():
         if arcs is None:
             arcs = []
             arcs = [set(arc) for arc in self.crossword.overlaps if self.crossword.overlaps[arc] is not None and set(arc) not in arcs]
-        print(arcs)
+            arcs = [tuple(arc) for arc in arcs]
+        for arc in arcs:
+            print(arc, self.crossword.overlaps[arc])
 
 
     def assignment_complete(self, assignment):
