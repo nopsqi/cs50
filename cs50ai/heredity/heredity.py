34a6bcd740e850b6d973042392229824c51aa4d3
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-19 06:54:44 +0000

    Thu, Oct 19, 2023, 1:54 PM +07:00

--- cs50ai/heredity/heredity.py
+++ cs50ai/heredity/heredity.py
@@ -151,7 +151,8 @@ def joint_probability(people, one_gene, two_genes, have_trait):
             return PROBS["gene"][person_gene] * PROBS["trait"][person_gene][person in have_trait]
 
         not_mutated = 1 - PROBS["mutation"]
-        case = {2: PROBS["mutation"] * PROBS["mutation"], 1: , 0: not_mutated * not_mutated}
+        case = {0: PROBS["mutation"] * PROBS["mutation"], 2: not_mutated * not_mutated}
+        case[1] = case[2]
 
         return PROBS["trait"][person_gene][person in have_trait] * (case[index(people[person]["father"])] + case[index(people[person]["mother"])])
         # return case[index(people[person]["father"])] + case[index(people[person]["mother"])]
