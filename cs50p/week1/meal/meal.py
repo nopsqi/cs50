870c78e407639462b919d5509f061499ea60a7b8
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-21 01:02:16 +0000

    Sat, Oct 21, 2023, 8:02 AM +07:00

--- cs50p/week1/meal/meal.py
+++ cs50p/week1/meal/meal.py
@@ -1,14 +1,25 @@
 def main():
-    ...
+    convert(input("What time is it? ").strip().lower())
 
 
 def convert(time):
-    hour = time.split(":")[0]
+    hour = int(time.split(":")[0])
     match time.split(" ")[-1]:
         case "a.m.":
             if 6 < hour < 9:
-                return "breakfast time"
-            if 
+                print("breakfast time")
+        case "p.m.":
+            if hour == 12 and hour == 1:
+                print("lunch time")
+            elif 5 < hour < 8:
+                print("dinner time")
+        case _:
+            if 6 < hour < 9:
+                print("breakfast time")
+            elif 11 < hour < 14:
+                print("lunch time")
+            elif 17 < hour < 20:
+                print("dinner time")
 
 
 if __name__ == "__main__":
