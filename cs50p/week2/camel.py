c852792a8149ad1e6110b38ff9d3b5851c4d4d19
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-21 12:34:32 +0000

    Sat, Oct 21, 2023, 7:34 PM +07:00

--- cs50p/week2/camel.py
+++ cs50p/week2/camel.py
@@ -1,5 +1,6 @@
-
-for s in input("camelCase: ").strip():
+camel_case = input("camelCase: ").strip()
+print("snake_case:", end=" ")
+for s in camel_case:
     if s.isupper():
         print(f"_{s.lower()}", end="")
         continue
