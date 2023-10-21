085964a0f30391d49e98e5f08c95bc7e4e36d67a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-21 13:07:37 +0000

    Sat, Oct 21, 2023, 8:07 PM +07:00

--- cs50p/week2/coke/coke.py
+++ cs50p/week2/coke/coke.py
@@ -3,7 +3,7 @@ print(f"Amount Due: {coin}")
 while True:
     amount = int(input("Insert Coin: "))
     if amount == coin:
-        print("Change Owed: 0}")
+        print("Change Owed: 0")
         break
     if amount > coin:
         print(f"Change Owed: {coin}")
