f795dde10df3499915c67fe4ecfc0790960be2eb
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-21 00:32:28 +0000

    Sat, Oct 21, 2023, 7:32 AM +07:00

--- cs50p/week1/extensions/extensions.py
+++ cs50p/week1/extensions/extensions.py
@@ -4,5 +4,12 @@ match input("File name: ").split(".")[-1]:
     case "jpg":
         print("image/jpeg")
     case "png":
+        print("image/png")
     case "pdf":
-    case "
+        print("application/pdf")
+    case "txt":
+        print("text/plain")
+    case "zip":
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
