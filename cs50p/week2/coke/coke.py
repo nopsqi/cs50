585ba2d592525516a549bfff8f2c30554d96e1ba
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-21 13:24:14 +0000

    Sat, Oct 21, 2023, 8:24 PM +07:00

--- cs50p/week2/coke/coke.py
+++ cs50p/week2/coke/coke.py
@@ -4,4 +4,10 @@ while True:
     coin = int(input("Insert Coin: "))
     if coin not in [5, 10, 25]:
         continue
-    if amount - coin 
+    amount -= coin
+    if amount == 0:
+        print(f"Change Owed: {amount}")
+        break
+    if amount < 0:
+        print(f"Change Owed: {amount + coin}")
+        break
