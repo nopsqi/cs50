e6f147680b5e203f4e2a0f5ccf540a7e91383127
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 16:15:44 +0000

    Sun, Oct 22, 2023, 11:15 PM +07:00

--- cs50ai/week4/shopping/test.py
+++ cs50ai/week4/shopping/test.py
@@ -4,7 +4,7 @@ from shopping import *
 def main():
     evidence, labels = load_data("shopping.csv")
     model = train_model(evidence, labels)
-    predictions = model.predict()
+    
 
 
 if __name__ == "__main__":
