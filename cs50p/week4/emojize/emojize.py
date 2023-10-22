973d95a136134726e13457a576c7e78808e63113
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 00:59:37 +0000

    Sun, Oct 22, 2023, 7:59 AM +07:00

--- cs50p/week4/emojize/emojize.py
+++ cs50p/week4/emojize/emojize.py
@@ -4,7 +4,7 @@ text = input("Input: ").split(":")
 print(text)
 
 for i, t in enumerate(text):
-    if i % 2 == 0:
+    if i % 2 == 1:
         print(emojize(f":{t}:"), end="")
     else:
         print(t, end="")
