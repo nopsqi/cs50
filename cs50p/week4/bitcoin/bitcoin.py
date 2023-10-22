ec7710786155763a2c63111e3054adbb2dee75de
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 03:35:19 +0000

    Sun, Oct 22, 2023, 10:35 AM +07:00

--- cs50p/week4/bitcoin/bitcoin.py
+++ cs50p/week4/bitcoin/bitcoin.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import sys
 
@@ -9,4 +10,4 @@ except ValueError:
     sys.exit("Command-line argument is not a number")
 
 response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-print(response.json())
+print(json.dumps(response.json(), indent=2))
