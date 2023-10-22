4fa65005d026f022c55ebe126bf3f0b1bcddf1a5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 01:51:01 +0000

    Sun, Oct 22, 2023, 8:51 AM +07:00

--- cs50p/week4/game/game.py
+++ cs50p/week4/game/game.py
@@ -2,4 +2,20 @@ import random
 
 while True:
     try:
-        number = random.randint(1, )
+        number = random.randint(1, int(input("Level: ")))
+    except ValueError:
+        continue
+    break
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+    except ValueError:
+        continue
+    if guess > number:
+        print("Too large!")
+    elif guess < number:
+        print("Too small!")
+    else:
+        print("Just right!")
+        break
