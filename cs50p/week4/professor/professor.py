8a46e4bafb32f8d699b1cee3ab1ab8ab913616ef
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 02:19:38 +0000

    Sun, Oct 22, 2023, 9:19 AM +07:00

--- cs50p/week4/professor/professor.py
+++ cs50p/week4/professor/professor.py
@@ -16,6 +16,7 @@ def main():
                 print("EEE")
                 continue
             break
+    print(f"Score: {sum(actual == answer for actual, answer in zip(actuals, answers))}")
 
 
 def get_level():
@@ -27,6 +28,7 @@ def get_level():
 
 
 def generate_integer(level):
+    level = level - 1
     return random.randint(10 ** level, (10 ** level) * 9)
 
 
