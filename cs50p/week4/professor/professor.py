c2d73ffcecded6247cb38030d82f8a2d579462f4
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 02:15:50 +0000

    Sun, Oct 22, 2023, 9:15 AM +07:00

--- cs50p/week4/professor/professor.py
+++ cs50p/week4/professor/professor.py
@@ -2,26 +2,32 @@ import random
 
 
 def main():
-    actual = []
+    actuals = []
     answers = []
     level = get_level()
     for i in range(10):
         x = generate_integer(level)
         y = generate_integer(level)
-        try:
-            answers.append(int(input(f"{x} + {y}")))
-        except ValueError:
-            print("EEE")
-            continue
-        
+        actuals.append(x + y)
+        while True:
+            try:
+                answers.append(int(input(f"{x} + {y}")))
+            except ValueError:
+                print("EEE")
+                continue
+            break
 
 
 def get_level():
-    ...
+    while True:
+        try:
+            return int(input("Level: "))
+        except ValueError:
+            pass
 
 
 def generate_integer(level):
-    ...
+    return random.randint(level ** 10, (level ** 10) * 9)
 
 
 if __name__ == "__main__":
