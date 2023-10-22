9b9ddcf5ffa0c1dbc3ebe487f1617fe7793d2096
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 02:44:40 +0000

    Sun, Oct 22, 2023, 9:44 AM +07:00

--- cs50p/week4/professor/professor.py
+++ cs50p/week4/professor/professor.py
@@ -8,14 +8,17 @@ def main():
     for i in range(10):
         x = generate_integer(level)
         y = generate_integer(level)
-        actuals[i] = x + y
+        actuals.append(x + y)
         tried = 0
         while True:
             if tried > 2:
                 print(f"{x} + {y} = {actuals[i]}")
                 break
+            answer = input(f"{x} + {y} = ")
             try:
-                answers[i] = int(input(f"{x} + {y} = "))
+                answers[i] = int(answer)
+            except IndexError:
+                answers.append(int(answer))
             except ValueError:
                 print("EEE")
                 tried += 1
