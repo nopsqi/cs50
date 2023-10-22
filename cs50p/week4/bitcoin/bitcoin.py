115cdc71fa1e97e447e9a3d83f951f666c830305
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 03:30:12 +0000

    Sun, Oct 22, 2023, 10:30 AM +07:00

--- cs50p/week4/bitcoin/bitcoin.py
+++ cs50p/week4/bitcoin/bitcoin.py
@@ -1,6 +1,11 @@
 import requests
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) != 2:
     sys.exit("Missing command-line argument")
-if not sys.argv[1].is
+try:
+    number = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+response = requests
