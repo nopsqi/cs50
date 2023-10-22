02d33de669f581dcbc019cf46d24718c6c1df705
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-22 01:20:36 +0000

    Sun, Oct 22, 2023, 8:20 AM +07:00

--- cs50p/week4/figlet/figlet.py
+++ cs50p/week4/figlet/figlet.py
@@ -1,6 +1,10 @@
 from pyfiglet import Figlet
 import sys
 
+if len(sys.argv) < 2:
+    sys.exit("usage: python figlet.py [-f, --f font]")
+if len(sys.argv) == 
+
 figlet = Figlet()
 
 print(figlet.getFonts())
