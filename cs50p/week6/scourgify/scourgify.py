f54ef985d284abf45961bce4c2d47cd2f2299e04
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 17:00:43 +0000

    Tue, Oct 24, 2023, 12:00 AM +07:00

---
+++ cs50p/week6/scourgify/scourgify.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if
+
+
+if __name__ == "__main__":
+    main()
