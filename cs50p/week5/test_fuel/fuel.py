d3aa95f992cd23882bf58a08dc993d3d1a64e0cd
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:34:52 +0000

    Mon, Oct 23, 2023, 8:34 PM +07:00

---
+++ cs50p/week5/test_fuel/fuel.py
@@ -0,0 +1,24 @@
+def main():
+    while True:
+        try:
+            x, y = input("Fraction: ").split("/")
+            z = int(x) / int(y)
+        except (ValueError, ZeroDivisionError):
+            pass
+        else:
+            if z > 1:
+                continue
+            if z < 0.02:
+                print("E")
+            elif z > 0.98:
+                print("F")
+            else:
+                print(f"{(z * 100):.0f}%")
+            break
+
+
+def convert()
+
+
+if __name__ == "__main__":
+    main()
