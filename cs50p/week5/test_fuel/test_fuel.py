994b4b0535eeeb0a73077684be5ec6edf72b7fd1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:57:43 +0000

    Mon, Oct 23, 2023, 8:57 PM +07:00

--- cs50p/week5/test_fuel/test_fuel.py
+++ cs50p/week5/test_fuel/test_fuel.py
@@ -6,4 +6,5 @@ def test_convert():
     assert convert("3/4") == 75
     assert convert("2/2") == 100
     assert convert("0/3") == 0
-    with pytest.raises(ValueError)
+    with pytest.raises(ValueError):
+        
