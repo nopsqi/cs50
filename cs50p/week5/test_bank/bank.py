b71145b9f17919922a19f45bb74f9d1904024439
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:05:43 +0000

    Mon, Oct 23, 2023, 8:05 PM +07:00

---
+++ cs50p/week5/test_bank/bank.py
@@ -0,0 +1,9 @@
+def main():
+    greet = input("Greeting: ").strip()
+
+    if greet.startswith("Hello"):
+        print("$0")
+    elif greet.startswith("H"):
+        print("$20")
+    else:
+        print("$100")
