f22320ff2aeb29fed6a5b076fb173a7eca26c325
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 16:22:44 +0000

    Mon, Oct 23, 2023, 11:22 PM +07:00

--- cs50p/week6/lines/lines.py
+++ cs50p/week6/lines/lines.py
@@ -6,7 +6,12 @@ def main():
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
-    if sys.argv[1]
+    if sys.argv[1].split(".")[-1] != "py":
+        sys.exit("Not a Python file")
+
+    try:
+        file = open(sys.argv[1])
+    except: 
 
 
 if __name__ == "__main__":
