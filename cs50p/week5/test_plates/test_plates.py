ed56cf40c9f9095d891b65e2086d44c55e35b436
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:23:41 +0000

    Mon, Oct 23, 2023, 8:23 PM +07:00

--- cs50p/week5/test_plates/test_plates.py
+++ cs50p/week5/test_plates/test_plates.py
@@ -3,4 +3,6 @@ from plates import is_valid
 
 def test_is_valid():
     assert is_valid("CS50") == True
-    assert is_valid(")
+    assert is_valid("CS05") == False
+    assert is_valid("CS50P") == False
+    
