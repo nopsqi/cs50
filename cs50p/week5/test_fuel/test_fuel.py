d9939fe7323a5e4ba0606f447b12e2f028c4b068
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 14:09:10 +0000

    Mon, Oct 23, 2023, 9:09 PM +07:00

--- cs50p/week5/test_fuel/test_fuel.py
+++ cs50p/week5/test_fuel/test_fuel.py
@@ -2,13 +2,10 @@ import pytest
 from fuel import convert, gauge
 
 
-def test_convert_normal():
+def test_convert():
     assert convert("3/4") == 75
     assert convert("2/2") == 100
     assert convert("0/3") == 0
-
-
-def test_convert_error():
     with pytest.raises(ValueError):
         convert("cat/2")
     with pytest.raises(ValueError):
