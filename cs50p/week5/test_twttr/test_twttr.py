e9ab696f71aed06a37fc08076fa7ddabb4ae90f1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:02:52 +0000

    Mon, Oct 23, 2023, 8:02 PM +07:00

--- cs50p/week5/test_twttr/test_twttr.py
+++ cs50p/week5/test_twttr/test_twttr.py
@@ -5,3 +5,4 @@ def test_shorten():
     assert shorten("Twitter") == "Twttr"
     assert shorten("What's your name?") == "Wht's yr nm?"
     assert shorten("CS50") == "CS50"
+    assert shorten("HELLO") == "HLL"
