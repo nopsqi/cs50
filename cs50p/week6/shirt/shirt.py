72552282e8f4c8947250b43ddd6a7989023835a4
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 17:29:32 +0000

    Tue, Oct 24, 2023, 12:29 AM +07:00

--- cs50p/week6/shirt/shirt.py
+++ cs50p/week6/shirt/shirt.py
@@ -1,6 +1,11 @@
+import os
 import sys
 
 
 def main():
     if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
+
+    for arg in sys.argv:
+        if os.split.text(arg)[-1].lower() not in [".jpg", ".jpeg", ".png"]:
+            sys.exit()
