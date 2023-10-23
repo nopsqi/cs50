dc5e480e82d5e7360c8cb2476a095623b0347195
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:25:43 +0000

    Mon, Oct 23, 2023, 8:25 PM +07:00

--- cs50p/week5/test_plates/test_plates.py
+++ cs50p/week5/test_plates/test_plates.py
@@ -5,4 +5,6 @@ def test_is_valid():
     assert is_valid("CS50") == True
     assert is_valid("CS05") == False
     assert is_valid("CS50P") == False
-    
+    assert is_valid("PI3.14") == False
+    assert is_valid("H") == False
+    assert is_valid("OUTATIME") == False
