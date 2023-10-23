ae69d96edec2dbdcf9bcc9adeb60a44a8433bf20
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 17:38:44 +0000

    Tue, Oct 24, 2023, 12:38 AM +07:00

--- cs50p/week6/shirt/shirt.py
+++ cs50p/week6/shirt/shirt.py
@@ -9,7 +9,10 @@ def main():
     if not is_valid(sys.argv[1]):
         sys.exit("Invalid File")
     extension = os.split.text(sys.argv[1])[-1].lower()
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
+        if not is_valid(arg):
+            sys.exit("Invalid File")
+        if os.split.text(arg)
 
 
 def is_valid(filename):
