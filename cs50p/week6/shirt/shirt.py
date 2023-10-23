f72e3b3a343fbaf383dc14337905224673bef8c3
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 17:34:43 +0000

    Tue, Oct 24, 2023, 12:34 AM +07:00

--- cs50p/week6/shirt/shirt.py
+++ cs50p/week6/shirt/shirt.py
@@ -6,7 +6,17 @@ def main():
     if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
 
-    extension = 
+    extension =
     for arg in sys.argv:
-        if os.split.text(arg)[-1].lower() not in [".jpg", ".jpeg", ".png"]:
+        if not is_valid(arg):
             sys.exit("Invalid input")
+
+
+def is_valid(filename):
+    if os.split.text(filename)[-1].lower() in [".jpg", ".jpeg", ".png"]:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    main()
