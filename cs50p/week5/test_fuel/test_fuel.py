493b4027e1c7832f228923b5343c6c6a50024056
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 14:05:19 +0000

    Mon, Oct 23, 2023, 9:05 PM +07:00

--- cs50p/week5/test_fuel/test_fuel.py
+++ cs50p/week5/test_fuel/test_fuel.py
@@ -25,5 +25,5 @@ def test_gauge():
     assert gauge(100) == "F"
     assert gauge(75) == "75%"
     assert gauge(1) == "E"
-    assert gauge() == "E"
+    assert gauge(0) == "E"
     assert gauge(110) == None
