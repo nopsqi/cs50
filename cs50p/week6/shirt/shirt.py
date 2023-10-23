72ac2d714d44ce14501e376fec74dba0fe7c7160
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 17:47:24 +0000

    Tue, Oct 24, 2023, 12:47 AM +07:00

--- cs50p/week6/shirt/shirt.py
+++ cs50p/week6/shirt/shirt.py
@@ -9,13 +9,13 @@ def main():
 
     if not is_valid(sys.argv[1]):
         sys.exit("Invalid File")
-    extension = os.split.text(sys.argv[1])[-1].lower()
+    extension = os.path.splitext(sys.argv[1])[-1].lower()
     images = []
 
     for arg in sys.argv[1:]:
         if not is_valid(arg):
             sys.exit("Invalid File")
-        if os.split.text(arg)[-1].lower() != extension:
+        if os.path.splitext(arg)[-1].lower() != extension:
             sys.exit("Input and output have diferent extensions")
 
         try:
@@ -25,7 +25,7 @@ def main():
 
 
 def is_valid(filename):
-    if os.split.text(filename)[-1].lower() in [".jpg", ".jpeg", ".png"]:
+    if os.path.splitext(filename)[-1].lower() in [".jpg", ".jpeg", ".png"]:
         return True
     return False
 
