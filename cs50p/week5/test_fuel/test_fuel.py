2d7ce19f487c11bc40f46834baecde5ad1840888
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:55:18 +0000

    Mon, Oct 23, 2023, 8:55 PM +07:00

--- cs50p/week5/test_fuel/test_fuel.py
+++ cs50p/week5/test_fuel/test_fuel.py
@@ -5,4 +5,5 @@ from fuel import convert, gauge
 def test_convert():
     assert convert("3/4") == 75
     assert convert("2/2") == 100
-    assert convert("0/)
+    assert convert("0/3") == 0
+    assert 
