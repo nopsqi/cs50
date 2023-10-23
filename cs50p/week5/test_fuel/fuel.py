dc33f45c05812a63f7f90273c5e0d6e9ebb870b4
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:42:44 +0000

    Mon, Oct 23, 2023, 8:42 PM +07:00

--- cs50p/week5/test_fuel/fuel.py
+++ cs50p/week5/test_fuel/fuel.py
@@ -4,21 +4,23 @@ def main():
             percent = convert(input("Fraction: "))
             status = gauge(percent)
         except (ValueError, ZeroDivisionError):
-            pass
-        else:
-            if z > 1:
-                continue
-            if z < 0.02:
-                print("E")
-            elif z > 0.98:
-                print("F")
-            else:
-                print(f"{(z * 100):.0f}%")
-            break
+            continue
+        
 
 
 def convert(fraction):
 
+
+def gauge(percentage):
+    if z > 1:
+        continue
+    if z < 0.02:
+        print("E")
+    elif z > 0.98:
+        print("F")
+    else:
+        print(f"{(z * 100):.0f}%")
+    break
 
 
 if __name__ == "__main__":
