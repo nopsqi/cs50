488a4fbb8b608bb93496dcb831b2a2dfdc574c23
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-23 13:21:43 +0000

    Mon, Oct 23, 2023, 8:21 PM +07:00

---
+++ cs50p/week5/test_plates/plates.py
@@ -0,0 +1,29 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if not s.isalnum():
+        return False
+    if not s[:2].isalpha():
+        return False
+    if not (1 < len(s) < 7):
+        return False
+    first = None
+    for l in s:
+        if l.isdigit():
+            first = l
+            break
+    if first == "0":
+        return False
+    if first is not None and not s[s.index(first):].isdigit():
+        return False
+
+    return True
+
+if __name__ == "__main__":
+    main()
