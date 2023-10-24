9f8356312aee61936811d8315e848260932b10b1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-24 16:35:40 +0000

    Tue, Oct 24, 2023, 11:35 PM +07:00

--- cs50ai/week5/traffic/traffic.py
+++ cs50ai/week5/traffic/traffic.py
@@ -95,6 +95,9 @@ def get_model():
         tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dropout(0.5),
 
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
     model.compile(
