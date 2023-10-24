ef02b10b751fd854a93b46983a4e68be6c35cf2e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-24 16:32:10 +0000

    Tue, Oct 24, 2023, 11:32 PM +07:00

--- cs50ai/week5/traffic/traffic.py
+++ cs50ai/week5/traffic/traffic.py
@@ -87,7 +87,7 @@ def get_model():
         tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-        tf.keras.layers.Conv2D(238, (3, 3), activation="relu"),
+        tf.keras.layers.Conv2D(128, (3, 3), activation="relu"),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
         tf.keras.layers.Flatten(),
