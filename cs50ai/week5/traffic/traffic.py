2ceb1e5358641c71f331e390e80a89c70c95cd19
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-24 15:26:23 +0000

    Tue, Oct 24, 2023, 10:26 PM +07:00

--- cs50ai/week5/traffic/traffic.py
+++ cs50ai/week5/traffic/traffic.py
@@ -80,7 +80,16 @@ def get_model():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(
             32, (3, 3), activation="relu", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
-        )
+        ),
+
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
     model.compile(
         optimizer="adam",
