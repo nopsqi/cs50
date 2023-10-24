3d2df11e70bb29f67b6359a88d823d303b64356c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-24 23:45:05 +0000

    Wed, Oct 25, 2023, 6:45 AM +07:00

--- cs50p/week7/numb3rs/numb3rs.py
+++ cs50p/week7/numb3rs/numb3rs.py
@@ -8,7 +8,7 @@ def main():
 
 def validate(ip):
     # return re.search(r"(([0=1][0-5]{2}|[0-9]{1,2})\.){2}[0=1][0-5]{2}|[0-9]{1,2}", ip) is not None
-    return re.search(r"[0=1][0-5]{2}", ip) is not None
+    return re.search(r"[0-2][0-5]{2}|[0-9]{1,2}", ip) is not None
 
 
 if __name__ == "__main__":
