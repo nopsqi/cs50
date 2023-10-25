1333e33cf20c1ac98830c39c72ba11f2e2a3607f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 14:20:11 +0000

    Wed, Oct 25, 2023, 9:20 PM +07:00

--- cs50p/week7/working/working.py
+++ cs50p/week7/working/working.py
@@ -10,7 +10,9 @@ def main():
 def convert(s):
     time = re.findall(r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)", s, re.IGNORECASE)
     for i, t in enumerate(time):
-        if t[0] 
+        if t[0] not in [5, 9]:
+            raise ValueError
+        
 
 
 if __name__ == "__main__":
