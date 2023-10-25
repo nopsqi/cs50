c590ae2cc7c49aeb123e396cddd75989dddf4f6e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 14:41:18 +0000

    Wed, Oct 25, 2023, 9:41 PM +07:00

--- cs50p/week7/working/working.py
+++ cs50p/week7/working/working.py
@@ -5,22 +5,29 @@ import sys
 def main():
     # print(convert(input("Hours: ")))
     convert("9:00 AM to 5 PM")
+    # convert("9:00 AM - 5 PM")
 
 
 def convert(s):
-    time = re.match(r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm) to (0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)", s, re.IGNORECASE)
-    print(time.groups())
+    if (
+        time := re.match(
+            r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm) to (0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)",
+            s,
+            re.IGNORECASE,
+        )
+    ) is None:
+        raise ValueError
+    time = 
     return
     for i, t in enumerate(time):
-
         hour = int(t[0])
         minute = int(t[1]) if t[1] else 0
         meridiem = t[2].lower()
 
         match meridiem:
-            case 'am':
+            case "am":
                 hour = hour if hour != 12 else 0
-            case 'pm':
+            case "pm":
                 hour = hour + 12 if hour != 12 else hour
 
         time[i] = f"{hour:02}:{minute:02}"
