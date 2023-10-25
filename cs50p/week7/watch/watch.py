b3ea54aa3c4da8eb96ca36ae3c892725389e1231
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 01:09:56 +0000

    Wed, Oct 25, 2023, 8:09 AM +07:00

--- cs50p/week7/watch/watch.py
+++ cs50p/week7/watch/watch.py
@@ -3,14 +3,16 @@ import sys
 
 
 def main():
-    # print(parse(input("HTML: ")))
+    print(parse(input("HTML: ")))
     # parse('<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>')
-    parse('<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+    # parse('<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+    # parse('<iframe src="https://www.cs50.com/embed/xvFZjo5PgG0"></iframe>')
 
 
 def parse(s):
-    link = re.match(r"^<iframe.*src=\".*you[^\"]+/([^\"/]+)\".*></iframe>$", s)
-    print(link.groups())
+    if not (link := re.match(r"^<iframe.*src=\".*you[^\"]+/([^\"/]+)\".*></iframe>$", s)):
+        return link
+    return f"https;//youtu.be/{link}"
 
 
 if __name__ == "__main__":
