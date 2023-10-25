2e52f7391e68bd0c6cc5d8423e0d24b038f97c3f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 00:58:10 +0000

    Wed, Oct 25, 2023, 7:58 AM +07:00

--- cs50p/week7/watch/watch.py
+++ cs50p/week7/watch/watch.py
@@ -3,12 +3,14 @@ import sys
 
 
 def main():
-    print(parse(input("HTML: ")))
+    # print(parse(input("HTML: ")))
+    # parse('<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>')
+    parse('<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
 
 
 def parse(s):
     link = re.match(r"^<iframe.*src=\"([^\"]+)\".*></iframe>$", s)
-    print(link.groups)
+    print(link.groups())
 
 
 if __name__ == "__main__":
