d561665f3a41abd33443d868aadbe854af8e8693
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 00:39:47 +0000

    Wed, Oct 25, 2023, 7:39 AM +07:00

--- cs50p/week7/watch/watch.py
+++ cs50p/week7/watch/watch.py
@@ -7,7 +7,7 @@ def main():
 
 
 def parse(s):
-    return re.search(r"^a(.+)z$", s).groups(1)
+    return re.search(r"^<iframe src=\"(.+)\"></iframe>$", s).groups(1)[0]
 
 
 if __name__ == "__main__":
