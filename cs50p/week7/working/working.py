55dfc7a0f0707283589042da6520e3a60344ac4f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 14:39:09 +0000

    Wed, Oct 25, 2023, 9:39 PM +07:00

--- cs50p/week7/working/working.py
+++ cs50p/week7/working/working.py
@@ -8,8 +8,8 @@ def main():
 
 
 def convert(s):
-    time = re.findall(r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)", s, re.IGNORECASE)
-    print(time)
+    time = re.match(r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm) to (0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)", s, re.IGNORECASE)
+    print(time.groups())
     return
     for i, t in enumerate(time):
 
