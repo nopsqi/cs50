8c6709a438d59ccd747a2b45c35be8764c87d09a
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 15:02:12 +0000

    Wed, Oct 25, 2023, 10:02 PM +07:00

--- cs50p/week7/working/working.py
+++ cs50p/week7/working/working.py
@@ -3,15 +3,15 @@ import sys
 
 
 def main():
-    print(convert(input("Hours: ")))
-    # print(convert("9:00 AM to 5 PM"))
+    # print(convert(input("Hours: ")))
+    print(convert("9:00 AM to 5 PM"))
     # convert("9:00 AM - 5 PM")
 
 
 def convert(s):
     if (
         re.match(
-            r"(0?(?:5|9))(?:\:00)? *(am|pm) to (0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)",
+            r"(0?(?:5|9))(?:\:00)? *(am|pm) to (0?(?:5|9))(?:\:00)? *(am|pm)",
             s,
             re.IGNORECASE,
         )
@@ -19,18 +19,9 @@ def convert(s):
     ):
         raise ValueError
 
-    time = re.findall(r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)", s, re.IGNORECASE)
+    time = re.findall(r"(0?(?:5|9))(?:\:00)? *(am|pm)", s, re.IGNORECASE)
     for i, t in enumerate(time):
-        hour = int(t[0])
-        meridiem = t[2].lower()
-
-        match meridiem:
-            case "am":
-                hour = hour if hour != 12 else 0
-            case "pm":
-                hour = hour + 12 if hour != 12 else hour
-
-        time[i] = f"{hour:02}:{minute:02}"
+        time[i] = f"{int(t[0]) if t[1] == 'AM' else int(t[0]) + 12:02}:00"
 
     return f"{time[0]} to {time[1]}"
 
