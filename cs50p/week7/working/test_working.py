bcd03d323c2866e1f63cfd58775c33190b08eded
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-25 14:53:05 +0000

    Wed, Oct 25, 2023, 9:53 PM +07:00

---
+++ cs50p/week7/working/test_working.py
@@ -0,0 +1,19 @@
+import pytest
+from working import convert
+
+
+def test_with_minute():
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+
+
+def test_without_minute():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+
+
+def test_mix():
+    assert convert("9:00 AM to 5 PM") == "09:00 to 17:00"
+
+
+def test_not_in_range():
+    with pytest.raises(ValueError):
+        convert("10)
--- cs50p/week7/working/working.py
+++ cs50p/week7/working/working.py
@@ -3,8 +3,8 @@ import sys
 
 
 def main():
-    # print(convert(input("Hours: ")))
-    convert("9:00 AM to 5 PM")
+    print(convert(input("Hours: ")))
+    # print(convert("9:00 AM to 5 PM"))
     # convert("9:00 AM - 5 PM")
 
 
@@ -17,12 +17,16 @@ def convert(s):
         )
         is None
     ):
-        raise ValueError
-    return
+        raise ValueError("Invallid input")
+
+    time = re.findall(r"(0?[0-9]|1[1-2])(?:\:([0-5][0-9]))? *(am|pm)", s, re.IGNORECASE)
     for i, t in enumerate(time):
         hour = int(t[0])
         minute = int(t[1]) if t[1] else 0
         meridiem = t[2].lower()
+
+        if hour not in [5, 9]:
+            raise ValueError("Hour must be 5 or 9")
 
         match meridiem:
             case "am":
@@ -31,7 +35,8 @@ def convert(s):
                 hour = hour + 12 if hour != 12 else hour
 
         time[i] = f"{hour:02}:{minute:02}"
-    print(time)
+
+    return f"{time[0]} to {time[1]}"
 
 
 if __name__ == "__main__":
