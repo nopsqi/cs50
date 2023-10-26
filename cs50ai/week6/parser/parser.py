17ee94991b4a661e3bdf6aed962580496ccccc38
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 17:09:01 +0000

    Fri, Oct 27, 2023, 12:09 AM +07:00

--- cs50ai/week6/parser/parser.py
+++ cs50ai/week6/parser/parser.py
@@ -15,12 +15,12 @@ V -> "smiled" | "tell" | "were"
 """
 
 NONTERMINALS = """
-S -> NP VP | NP | VP
+S -> NP VP | NP | VP | S Conj S | S Conj VP
 
 AP -> Adj | AP AP
-NP -> N | Det NP | AP NP | N PP
+NP -> N | Det NP | AP NP | N PP | NP Conj NP
 PP -> P NP | Adv NP
-VP -> V | V NP | V NP PP | V PP
+VP -> V | V NP | V PP | V NP PP
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
