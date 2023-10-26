6435418dd6888a858bf150aa3dc482c1910b4d3c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 14:44:29 +0000

    Thu, Oct 26, 2023, 9:44 PM +07:00

--- cs50p/week8/shirtificate/shirtificate.py
+++ cs50p/week8/shirtificate/shirtificate.py
@@ -5,11 +5,10 @@ def main():
     pdf = FPDF(orientation="portrait", format="a4")
     pdf.set_font("Helvetica", size=30)
     # pdf.set_text_color(255, 255, 255)
-    pdf.set_page_background("shirtificate.png")
     pdf.add_page()
     pdf.set_y(pdf.h / 2)
-    cell = pdf.cell(w=pdf.w, text="John Harvard took CS50", border=1, align="C")
-    print(type(cell))
+    pdf.cell(w=(pdf.w - pdf.l_margin - pdf.r_margin), text="John Harvard took CS50", border=1, align="C")
+    print(pdf.l_margin)
     pdf.output("shirtificate.pdf")
 
 
