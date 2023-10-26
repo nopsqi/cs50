f2c09a3541b20e2f5cba914016d757ea2a60d374
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 14:49:40 +0000

    Thu, Oct 26, 2023, 9:49 PM +07:00

--- cs50p/week8/shirtificate/shirtificate.py
+++ cs50p/week8/shirtificate/shirtificate.py
@@ -6,7 +6,7 @@ def main():
     pdf.set_font("Helvetica", size=30)
     # pdf.set_text_color(255, 255, 255)
     pdf.add_page()
-    pdf.image("shirtificate.png", x=Align.C)
+    pdf.image("shirtificate.png", x=Align.C, keep_aspect_ration=True)
     pdf.set_y(pdf.h / 2)
     pdf.cell(w=(pdf.w - pdf.l_margin - pdf.r_margin), text="John Harvard took CS50", border=1, align="C")
     print(pdf.l_margin)
