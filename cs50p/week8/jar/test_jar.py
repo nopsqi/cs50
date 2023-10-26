526065280c0785ecd78625ae37b6b45e7b390b0e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 13:14:53 +0000

    Thu, Oct 26, 2023, 8:14 PM +07:00

---
+++ cs50p/week8/jar/test_jar.py
@@ -0,0 +1,24 @@
+from jar import Jar
+
+
+def test_init():
+    jar = Jar(25)
+    assert jar.capacity == 25
+    assert jar.size == 0
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    jar = Jar()
+
+
+def test_withdraw():
+    ...
