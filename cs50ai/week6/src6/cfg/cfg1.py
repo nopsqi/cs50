f433cdf4d99561d4d1ecff547b139fc46deb37a6
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 16:43:50 +0000

    Thu, Oct 26, 2023, 11:43 PM +07:00

--- cs50ai/week6/src6/cfg/cfg1.py
+++ cs50ai/week6/src6/cfg/cfg1.py
@@ -3,7 +3,7 @@ import nltk
 grammar = nltk.CFG.fromstring("""
     S -> NP VP
 
-    AP -> A | A AP
+    AP -> A | AP AP
     NP -> N | D NP | AP NP | N PP
     PP -> P NP
     VP -> V | V NP | V NP PP
