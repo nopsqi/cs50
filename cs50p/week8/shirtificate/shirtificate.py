0bfd336aeeebb36e3064f80f96184c8e23e8e9c9
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 14:10:26 +0000

    Thu, Oct 26, 2023, 9:10 PM +07:00

--- cs50p/week8/shirtificate/shirtificate.py
+++ cs50p/week8/shirtificate/shirtificate.py
@@ -3,7 +3,7 @@ from fpdf import FPDF, XPos
 
 def main():
     pdf = FPDF(orientation="portrait", format="a4")
-    pdf.set_font("Helvetica", size=36)
+    pdf.set_font("Helvetica", size=30)
     # pdf.set_text_color(255, 255, 255)
     pdf.set_page_background("shirtificate.png")
     pdf.add_page()
