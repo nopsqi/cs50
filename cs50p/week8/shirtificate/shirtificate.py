2e61cee0cba898265bf22a71ae3d488246c48bc2
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-26 13:48:23 +0000

    Thu, Oct 26, 2023, 8:48 PM +07:00

--- cs50p/week8/shirtificate/shirtificate.py
+++ cs50p/week8/shirtificate/shirtificate.py
@@ -4,9 +4,10 @@ from fpdf import FPDF
 def main():
     pdf = FPDF(orientation="portrait", format="a4")
     pdf.set_font("Helvetica", size=36)
+    pdf.set_text_color(255, 255, 255)
     pdf.set_page_background("shirtificate.png")
     pdf.add_page()
-    pdf.cell(text="John Harvard took CS50")
+    pdf.cell(text="John Harvard took CS50", new_y="BOTTOM")
     pdf.output("shirtificate.pdf")
 
 
