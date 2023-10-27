29998924905b0d1bf61cb25879f2e20738cd0910
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-27 13:54:08 +0000

    Fri, Oct 27, 2023, 8:54 PM +07:00

--- cs50ai/week6/attention/mask.py
+++ cs50ai/week6/attention/mask.py
@@ -57,7 +57,8 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
-    print(tuple([round(255 * attention_score)] * 3))
+    print(dir(attention_score))
+    # print(tuple([round(255 * attention_score)] * 3))
     raise Exception
 
 
