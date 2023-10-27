2ba752d26410ba82d917c2e55c973632791455f1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-27 00:01:11 +0000

    Fri, Oct 27, 2023, 7:01 AM +07:00

--- cs50ai/week6/attention/mask.py
+++ cs50ai/week6/attention/mask.py
@@ -45,6 +45,7 @@ def get_mask_token_index(mask_token_id, inputs):
     Return the index of the token with the specified `mask_token_id`, or
     `None` if not present in the `inputs`.
     """
+    print(inputs)
     for i in inputs:
         print(i)
     raise NotImplementedError
