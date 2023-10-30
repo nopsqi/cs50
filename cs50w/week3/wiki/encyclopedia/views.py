30c82b3e69c39a49550d642937f169d2621643bf
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-10-30 23:25:39 +0000

    Tue, Oct 31, 2023, 6:25 AM +07:00

--- cs50w/week3/wiki/encyclopedia/views.py
+++ cs50w/week3/wiki/encyclopedia/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 
 def entry_page(request, title):
-    if not (entry := util.get_entry(title.capitalize())):
+    if not (entry := util.get_entry(title)):
         raise Http404("Entry doesn't exist")
     return render(request, f"encyclopedia/entry_page.html", {
         "entry": entry
