7bab94d1e17826090fa3da84dc7cd82d119bbfa5
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-01 23:00:14 +0000

    Thu, Nov 2, 2023, 6:00 AM +07:00

--- cs50w/week4/commerce/auctions/factories.py
+++ cs50w/week4/commerce/auctions/factories.py
@@ -2,9 +2,19 @@ import factory
 from .models import User, Category, Listing, Bid, BidHistory, Comment
 
 
+def main():
+    for _ in range(10):
+        
+
+
 class UserFactory(factory.django.DjangoFactory):
     class Meta:
         model = User
 
     username = factory.Faker("user_name")
-    email = 
+    email = factory.Faker("email")
+    password = "asdf"
+
+
+if __name__ == "__main__":
+    main()
