77599fa57f7be772aa0340d6db04ec22b86c4064
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-02 00:06:12 +0000

    Thu, Nov 2, 2023, 7:06 AM +07:00

--- cs50w/week4/commerce/auctions/models.py
+++ cs50w/week4/commerce/auctions/models.py
@@ -9,6 +9,9 @@ class User(AbstractUser):
 class Category(models.Model):
     name = models.CharField(max_length=16)
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         verbose_name_plural = "Categories"
 
