5ebbfa42e4ab7273d4bf96c431981a627205da2b
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-02 21:52:42 +0000

    Fri, Nov 3, 2023, 4:52 AM +07:00

--- cs50w/week4/commerce/auctions/models.py
+++ cs50w/week4/commerce/auctions/models.py
@@ -38,19 +38,6 @@ class Bid(models.Model):
     amount = models.DecimalField(max_digits=11, decimal_places=2, validators=[MinValueValidator(0.01)])
 
 
-class BidHistory(models.Model):
-    timestamp = models.DateField(auto_now_add=True)
-    bid = models.ForeignKey(Bid, on_delete=models.DO_NOTHING, related_name="histories")
-    amount = models.DecimalField(max_digits=11, decimal_places=2)
-
-    class Meta:
-        verbose_name = "Bid History"
-        verbose_name_plural = "Bid Histories"
-
-    def __str__(self):
-        f"{self.timestamp} {self.bid.user.username} bid {self.amount}"
-
-
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
