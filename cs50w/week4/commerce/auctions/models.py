6924c07e1aaedd7c8ba106c1999722f8ec274ee4
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-02 00:37:31 +0000

    Thu, Nov 2, 2023, 7:37 AM +07:00

--- cs50w/week4/commerce/auctions/models.py
+++ cs50w/week4/commerce/auctions/models.py
@@ -26,13 +26,15 @@ class Listing(models.Model):
 
 
 class Bid(models.Model):
+    timestamp = models.DateField(auto_now=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     ammount = models.DecimalField(max_digits=11, decimal_places=2)
 
 
-class BidHistory(Bid):
+class BidHistory(models.Model):
     timestamp = models.DateField(auto_now_add=True)
+    bid_id = models.ForeignKey(Bid, on_delete=models.DO_NOTHING, related_name="histories")
 
     class Meta:
         verbose_name = "Bid History"
