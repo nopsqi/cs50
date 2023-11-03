4a84b9abb383e5c34d30377ccf276c8019fdae12
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-03 15:07:41 +0000

    Fri, Nov 3, 2023, 10:07 PM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.core.validators import MinValueValidator
@@ -154,4 +154,4 @@ def delete(request):
     if request.user == listing.user:
         listing.delete()
         return HttpResponseRedirect(reverse('listings', args=[request.user]))
-    return
+    return HttpResponseForbidden()
