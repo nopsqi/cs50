af73ec42492c10a80d5914d05be0bbe2472692be
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-03 14:27:41 +0000

    Fri, Nov 3, 2023, 9:27 PM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -152,4 +152,5 @@ def delete(request):
     listing = get_object_or_404(Listing, id=request.GET.get("id"))
     if request.user == listing.user:
         listing.delete()
+        return HttpResponseRedirect(reverse(""))
 
