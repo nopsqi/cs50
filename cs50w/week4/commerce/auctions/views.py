41d865a60f2b866c86b7301f4043b67185871c75
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-04 16:08:10 +0000

    Sat, Nov 4, 2023, 11:08 PM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.core.validators import MinValueValidator
