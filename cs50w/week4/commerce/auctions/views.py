40dd765d5720f65d86de9a86b176126c873a4414
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-04 00:35:36 +0000

    Sat, Nov 4, 2023, 7:35 AM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -38,6 +38,7 @@ class BidForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         listing = kwargs.pop("listing", None)
+        request = kwargs.pop("request", None)
         super(BidForm, self).__init__(*args, **kwargs)
         for field in iter(self.fields):
             self.fields[field].label = ""
