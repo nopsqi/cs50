3d1f0edd195987569771ef57983d4ff115868c34
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-04 21:52:10 +0000

    Sun, Nov 5, 2023, 4:52 AM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -186,7 +186,7 @@ def add_watchlist(request):
     listing = get_object_or_404(Listing, id=request.POST.get("id"))
     if listing.user == request.user:
         return HttpResponseRedirect(f"{request.POST.get('prev', reverse('index'))}")
-    watchlist = Watchlist(user=request.user, listing=listing)
+    request.user.watchlist.
 
 
 @login_required(login_url="login")
