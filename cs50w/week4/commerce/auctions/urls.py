8a9662523999b1864b744aeffbd9c0a7fe19683b
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-04 21:57:57 +0000

    Sun, Nov 5, 2023, 4:57 AM +07:00

--- cs50w/week4/commerce/auctions/urls.py
+++ cs50w/week4/commerce/auctions/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create", views.create, name="create"),
     path("watchlist", views.watchlist, name="watchlist"),
+    path("watchlist/add", views.add_watchlist, )
     path("listing", views.listing, name="listing"),
     path("listing/close", views.close, name="close"),
     path("listing/delete", views.delete, name="delete"),
