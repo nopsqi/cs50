55a192076b6ad1a0efe2dd7068d4d790ef83649d
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-04 14:27:36 +0000

    Sat, Nov 4, 2023, 9:27 PM +07:00

--- cs50w/week4/commerce/auctions/factories.py
+++ cs50w/week4/commerce/auctions/factories.py
@@ -97,7 +97,7 @@ if Listing.objects.all().count() == 0:
 
 if Watchlist.objects.all().count() == 0:
     for user in User.objects.all():
-        
+        for _ in range()
 
 if Bid.objects.all().count() == 0:
     for listing in Listing.objects.all():
