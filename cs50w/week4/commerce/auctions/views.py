806ce3e85da941242d692f049e230f6f267135ba
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-05 00:59:45 +0000

    Sun, Nov 5, 2023, 7:59 AM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -300,4 +300,4 @@ class comment:
                 "bid_form": bid_form,
                 "comment_form": comment_form,
             })
-        return HttpResponseRedirect(f"{request.POST.get('prev')}")
+        return HttpResponseRedirect(f"{reverse('listing.show')}?id=listing.id")
