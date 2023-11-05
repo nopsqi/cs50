87a3ef879861f1aca75f1097af74fc0106293442
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-05 21:50:56 +0000

    Mon, Nov 6, 2023, 4:50 AM +07:00

--- cs50w/week4/commerce/auctions/views.py
+++ cs50w/week4/commerce/auctions/views.py
@@ -33,7 +33,7 @@ class ListingForm(forms.ModelForm):
                     "class": "form-control"
                 }
             )
-        self.fields["url"].widget.attrs["value"] = ""
+        self.fields["url"].widget = forms.UrlField()
 
 
 class BidForm(forms.ModelForm):
