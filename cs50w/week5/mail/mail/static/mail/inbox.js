e52024d92e51bce2a1d0c658822dc5d6ab069a7c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-06 23:42:25 +0000

    Tue, Nov 7, 2023, 6:42 AM +07:00

--- cs50w/week5/mail/mail/static/mail/inbox.js
+++ cs50w/week5/mail/mail/static/mail/inbox.js
@@ -30,4 +30,6 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch(')
 }
