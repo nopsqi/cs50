747ff9eb929349f633b1edf16916d4652eb82218
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-06 22:09:13 +0000

    Tue, Nov 7, 2023, 5:09 AM +07:00

--- cs50w/week5/mail/mail/factoies.py
+++ cs50w/week5/mail/mail/factoies.py
@@ -23,4 +23,4 @@ class EmailFactory(DjangoEmailFactory):
     subject = factory.Faker("sentence", nb_words=random.randint(1, 4))
     body = factory.Faker("text")
     timestamp = factory.Faker("date_time_between", start_date="-1y", end_date="now", tzinfo=timezone.get_current_timezone())
-    read = factory.Lazy
+    read = factory.LazyAttribute(lambda o: Tru)
