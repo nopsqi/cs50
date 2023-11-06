7a6846b85b95474078cb8fa052bacef5a742df8e
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-06 22:42:04 +0000

    Tue, Nov 7, 2023, 5:42 AM +07:00

--- cs50w/week5/mail/mail/factories.py
+++ cs50w/week5/mail/mail/factories.py
@@ -9,8 +9,7 @@ class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Faker("user_name")
-    email = factory.Faker("email")
+    username = factory.Faker("email")
     password = "pbkdf2_sha256$600000$Nhn7kL55O0rKMEzGd59oTm$9XFB9m6u+Ro8fThSWtvpdyBNQ9Rle+f/40Aq0k92vDg="
 
 
