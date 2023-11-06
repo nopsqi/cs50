7879c56cac2f801956d99e998e5cc8e406b2241f
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-06 21:40:46 +0000

    Tue, Nov 7, 2023, 4:40 AM +07:00

--- cs50w/week5/mail/mail/factoies.py
+++ cs50w/week5/mail/mail/factoies.py
@@ -1,6 +1,18 @@
-from mail.models import Email
+from mail.models import User, Email
 import factory
 from factory.django import DjangoModelFactory
 
 
+class UserFactory(DjangoEmailFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+
+
 class EmailFactory(DjangoEmailFactory):
+    class Meta:
+        model = Email
+
+    user = factory.SubFactory()
