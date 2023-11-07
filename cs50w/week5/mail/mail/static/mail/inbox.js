9066ca5c7b92390285d9ab097380ef1e1e8e8b05
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-07 01:42:07 +0000

    Tue, Nov 7, 2023, 8:42 AM +07:00

--- cs50w/week5/mail/mail/static/mail/inbox.js
+++ cs50w/week5/mail/mail/static/mail/inbox.js
@@ -44,7 +44,7 @@ function load_mailbox(mailbox) {
                 <div class="col-md-3 font-weight-bold">${item.sender}</div>
                 <div class="col-md-2">${item.subject.slice(0, 20)}</div>
                 <div class="col-md-5">${item.body.slice(0, 50)}</div>
-                <div dlass="col-md-2">${item.timestamp}</div>
+                <div dlass="col-md-2 text-">${item.timestamp}</div>
             </div>
         `
         Array.from(div.querySelector('.row').children).forEach(item => {
