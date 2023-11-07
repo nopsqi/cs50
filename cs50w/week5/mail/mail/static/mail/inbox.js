bd50b389b38b2e3ea56ad8bc3420d255bbdd3d97
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-07 02:29:58 +0000

    Tue, Nov 7, 2023, 9:29 AM +07:00

--- cs50w/week5/mail/mail/static/mail/inbox.js
+++ cs50w/week5/mail/mail/static/mail/inbox.js
@@ -2,48 +2,48 @@ const HOST = window.location.host
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+    // Use buttons to toggle between views
+    document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+    document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+    document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+    document.querySelector('#compose').addEventListener('click', compose_email);
 
-  // By default, load the inbox
-  load_mailbox('inbox');
+    // By default, load the inbox
+    load_mailbox('inbox');
 });
 
 function compose_email() {
 
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+    // Show compose view and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+    // Clear out composition fields
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
 }
 
 function load_mailbox(mailbox) {
 
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+    // Show the mailbox and hide other views
+    document.querySelector('#emails-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+    // Show the mailbox name
+    document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  fetch(`/emails/${mailbox}`)
-  .then(data => data.json())
-  .then(data => {
-    data.forEach(item => {
-        const div = document.createElement("div")
-        const readIndicator = "bg-light"
-        div.classList.add("card", "p-2")
-        if (item.read) {
-            div.classList.add(readIndicator)
-        }
-        div.innerHTML = `
+    fetch(`/emails/${mailbox}`)
+        .then(data => data.json())
+        .then(data => {
+            data.forEach(item => {
+                const div = document.createElement("div")
+                const readIndicator = "bg-light"
+                div.classList.add("card", "p-2")
+                if (item.read) {
+                    div.classList.add(readIndicator)
+                }
+                div.innerHTML = `
             <div class="row">
                 <div class="col-md-3 font-weight-bold">${item.sender}</div>
                 <div class="col-md-2">${item.subject.slice(0, 20)}</div>
@@ -51,29 +51,29 @@ function load_mailbox(mailbox) {
                 <div dlass="col-md-2 text-right">${item.timestamp}</div>
             </div>
         `
-        Array.from(div.querySelector('.row').children).forEach(item => {
-            item.classList.add("card-text", "overflow-auto")
+                Array.from(div.querySelector('.row').children).forEach(item => {
+                    item.classList.add("card-text", "overflow-auto")
+                })
+                const hoverIndicator = "alert-secondary"
+                div.onmouseenter = function(item) {
+                    this.classList.add(hoverIndicator)
+                    this.classList.remove(readIndicator)
+                }
+                div.onmouseleave = function() {
+                    this.classList.remove(hoverIndicator)
+                    if (item.read) {
+                        this.classList.add(readIndicator)
+                    }
+                }
+                div.onclick = () => load_mail(item.id)
+                document.querySelector('#emails-view').append(div);
+            });
         })
-        const hoverIndicator = "alert-secondary"
-        div.onmouseenter = function(item) {
-            this.classList.add(hoverIndicator)
-            this.classList.remove(readIndicator)
-        }
-        div.onmouseleave = function() {
-            this.classList.remove(hoverIndicator)
-            if (item.read) {
-                this.classList.add(readIndicator)
-            }
-        }
-        div.onclick = () => load_mail(item.id)
-        document.querySelector('#emails-view').append(div);
-    });
-  })
-  .catch(error => {
-    console.log(error)
-  });
+        .catch(error => {
+            console.log(error)
+        });
 }
 
 function load_mail(id) {
-    console.log(this)
+    document.querySelector('#emails-view')
 }
