50ff129ec2dc905c447658f971f67d020586c6c7
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-07 04:46:03 +0000

    Tue, Nov 7, 2023, 11:46 AM +07:00

--- cs50w/week5/mail/mail/static/mail/inbox.js
+++ cs50w/week5/mail/mail/static/mail/inbox.js
@@ -28,7 +28,7 @@ function compose_email() {
         fetch('/emails', {
             method: 'POST',
             body: JSON.stringify({
-                recipients: document.querySelector('#compose-recipients').value.split(","),
+                recipients: document.querySelector('#compose-recipients').value,
                 subject: document.querySelector('#compose-subject').value,
                 body: document.querySelector('#compose-body').value
             })
