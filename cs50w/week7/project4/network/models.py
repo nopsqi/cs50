ea3cfdc89ae0ed12404895148423f5eb62ea1098
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-08 15:30:15 +0000

    Wed, Nov 8, 2023, 10:30 PM +07:00

--- cs50w/week7/project4/network/models.py
+++ cs50w/week7/project4/network/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
+    
