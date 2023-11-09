8ed5bafce311cda71c6c658c8449d1a45a717d45
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-09 22:49:29 +0000

    Fri, Nov 10, 2023, 5:49 AM +07:00

--- cs50w/week7/project4/network/static/network/profile.js
+++ cs50w/week7/project4/network/static/network/profile.js
@@ -1,5 +1,7 @@
 const Profile = () => {
-    
+    const [state, setState] = React.useState({
+        url: new URL(window.location.pathname, window.location.origin)
+    })
     return (
         <div>
             THIS IS PROFILE
