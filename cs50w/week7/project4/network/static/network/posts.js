e52a6cb6e61cfcb81163cc3a6beaa918cc9ed852
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-10 03:56:43 +0000

    Fri, Nov 10, 2023, 10:56 AM +07:00

--- cs50w/week7/project4/network/static/network/posts.js
+++ cs50w/week7/project4/network/static/network/posts.js
@@ -85,6 +85,21 @@ const Posts = () => {
     );
 }
 
+const NewPost = () => {
+    const [state, setState] = React.useState({value: ""})
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <form className="mt-3 text-right" onSubmit={onSubmit}>
+            <textarea type="text" className="form-control" value={state.value} onChange={(e) => {setState({value: e.target.value})}}></textarea>
+            <button type="submit" className="mt-2 btn btn-primary">Post</button>
+        </form>
+    )
+}
+
 const Paginator = (props) => {
     const [state, setState] = React.useState(props);
 
