9be69dfb8647c12bc8abf1b9887b4adb09dadbbe
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-10 03:42:19 +0000

    Fri, Nov 10, 2023, 10:42 AM +07:00

--- cs50w/week7/project4/network/static/network/new-post.js
+++ cs50w/week7/project4/network/static/network/new-post.js
@@ -1,6 +1,6 @@
 const NewPost = () => {
     return (
-        <form className="mt-3">
+        <form className="mt-3 text-right">
             <textarea className="form-control"></textarea>
             <button className="mt-2 btn btn-primary">Post</button>
         </form>
