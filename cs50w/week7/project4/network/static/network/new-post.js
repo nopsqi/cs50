55ff55724d451f4b5aee8a5475e0a77d341811bf
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-10 03:46:07 +0000

    Fri, Nov 10, 2023, 10:46 AM +07:00

--- cs50w/week7/project4/network/static/network/new-post.js
+++ cs50w/week7/project4/network/static/network/new-post.js
@@ -1,7 +1,9 @@
 const NewPost = () => {
+    const [state, setState] = React.useState({value: ""})
+
     return (
         <form className="mt-3 text-right">
-            <textarea className="form-control"></textarea>
+            <textarea type="text" className="form-control" onChange={(e) => {setState({value: e.target.value})}} value={state.value}></textarea>
             <button className="mt-2 btn btn-primary">Post</button>
         </form>
     )
