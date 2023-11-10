537631a537e328c9e5013a5bae1b12d957969d0c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-10 03:49:35 +0000

    Fri, Nov 10, 2023, 10:49 AM +07:00

--- cs50w/week7/project4/network/static/network/new-post.js
+++ cs50w/week7/project4/network/static/network/new-post.js
@@ -6,8 +6,8 @@ const NewPost = () => {
     }
 
     return (
-        <form className="mt-3 text-right">
-            <textarea type="text" className="form-control" value={state.value}></textarea>
+        <form className="mt-3 text-right" onSubmit={onSubmit}>
+            <textarea type="text" className="form-control" value={state.value} onChange={(e) => {setState({value: e.target.value})}}></textarea>
             <button type="submit" className="mt-2 btn btn-primary">Post</button>
         </form>
     )
