e4ffa14f68858ae8884c60a38c190f22d280fa2c
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-10 06:25:59 +0000

    Fri, Nov 10, 2023, 1:25 PM +07:00

--- cs50w/week7/project4/network/views.py
+++ cs50w/week7/project4/network/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
