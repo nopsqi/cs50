c1654ccc38bb6b2669a787bb6b0706532efbf482
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-11 02:23:59 +0000

    Sat, Nov 11, 2023, 9:23 AM +07:00

--- cs50w/week7/project4/network/static/network/App.js
+++ cs50w/week7/project4/network/static/network/App.js
@@ -81,7 +81,7 @@ const App = () => {
     return (
         <div>
             <h1>APP HOLDER</h1>
-            <h2>{state.api.href}</h2>
+            <Modal />>
             <NewPost onClick={addPost} />
             <Posts posts={state.posts} onClick={deletePost} />
             <Paginator pages={state.pages} page={state.page} onClick={goToPage} />
@@ -138,21 +138,21 @@ const Paginator = (props) => {
 
 const Modal = (props) => {
     return (
-        <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <div className="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
+                    <div className="modal-body">
                         ...
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Understood</button>
                     </div>
                 </div>
             </div>
