23ddbf38f1d8d2fc0cd5b95257122b866705d039
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-11-11 16:37:56 +0000

    Sat, Nov 11, 2023, 11:37 PM +07:00

--- cs50w/week7/project4/network/static/network/App.js
+++ cs50w/week7/project4/network/static/network/App.js
@@ -63,6 +63,8 @@ const App = () => {
     }
 
     const deletePost = (e, id) => {
+        e.preventDefault()
+        console.log(id)
         fetch('/api/post', {
             method: 'DELETE',
             body: JSON.stringify({
