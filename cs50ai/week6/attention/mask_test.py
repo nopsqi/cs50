4e79714fa7080db98d7d1a76efecbe514d729af0
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-12-06 06:39:09 +0000

    Wed, Dec 6, 2023, 1:39 PM +07:00

--- cs50ai/week6/attention/mask_test.py
+++ cs50ai/week6/attention/mask_test.py
@@ -4,5 +4,5 @@ from tensorflow.tsl.python.lib.core import pywrap_ml_dtypes
 inputs = {}
 
 def test_get_mask_token_index():
-    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102], dtype=pywrap_ml_dtypes)
+    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102])
     assert get_mask_token_index(103, inputs) == 6
