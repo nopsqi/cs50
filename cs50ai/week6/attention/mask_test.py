779b53ba6b42d4f2b01440f511827c71bc209735
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-12-06 07:40:25 +0000

    Wed, Dec 6, 2023, 2:40 PM +07:00

--- cs50ai/week6/attention/mask_test.py
+++ cs50ai/week6/attention/mask_test.py
@@ -1,10 +1,13 @@
 from mask import *
 from transformers import AutoTokenizer, TFBertForMaskedLM
 
+inputs = {}
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
 def test_get_mask_token_index():
-    assert get_mask_token_index(tokenizer.mask_token_id, tokenizer("then i picked up a [MASK] from the table.", return_tensors="tf")) == 6
+    # assert get_mask_token_index(tokenizer.mask_token_id, tokenizer("then i picked up a [MASK] from the table.", return_tensors="tf")) == 6
+    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102])
+    assert get_mask_token_index(tokenizer.mask_token_id, inputs) == 6
 
 def test_get_mask_token_index_1():
     assert get_mask_token_index(tokenizer.mask_token_id, tokenizer("then i [MASK] up a book from the table.", return_tensors="tf")) == 2
