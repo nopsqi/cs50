5227de901ba9b4f899b2cc042b1d7aa7140b2398
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-12-06 06:07:39 +0000

    Wed, Dec 6, 2023, 1:07 PM +07:00

--- cs50ai/week6/attention/mask.py
+++ cs50ai/week6/attention/mask.py
@@ -47,10 +47,10 @@ def get_mask_token_index(mask_token_id, inputs):
     """
     # TODO: Implement this function
     print(inputs["input_ids"])
-    raise NotImplementedError
+    print(mask_token_id)
     if index := tf.where(inputs["input_ids"].numpy()[0] == mask_token_id).numpy():
-        return index.item()
-    return None
+    #     return index.item()
+    # return None
 
 
 
