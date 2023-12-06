ee3e3dc1294ed27514a1b4caea37a356ac3dc461
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-12-06 07:50:12 +0000

    Wed, Dec 6, 2023, 2:50 PM +07:00

--- cs50ai/week6/attention/mask_test.py
+++ cs50ai/week6/attention/mask_test.py
@@ -9,10 +9,10 @@ def test_get_mask_token_index():
 
 def test_get_mask_token_index_1():
     inputs["input_ids"] = tf.constant([[101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102]])
-    assert get_mask_token_index(mask_token_id, tokenizer("then i [MASK] up a book from the table.", return_tensors="tf")) == 2
+    assert get_mask_token_index(mask_token_id, inputs) == 2
 
 def test_get_mask_token_index_none():
-    inputs["input_ids"] = tf.constant([[101, 2059, 1045, 103, 2039, 1037, 2338, 2013, 1996, 2795, 1012, 102]])
+    inputs["input_ids"] = tf.constant([[101, 2059, 1045, 3856, 2039, 1037, 2338, 2013, 1996, 2795, 1012, 102]])
     assert get_mask_token_index(mask_token_id, inputs) == None
 
 def test_get_color_for_attention_score_0():
