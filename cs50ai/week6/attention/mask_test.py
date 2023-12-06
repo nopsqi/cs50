edfda7956738967bbec8ef3b52c1fd45f8011a97
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-12-06 07:43:25 +0000

    Wed, Dec 6, 2023, 2:43 PM +07:00

--- cs50ai/week6/attention/mask_test.py
+++ cs50ai/week6/attention/mask_test.py
@@ -2,18 +2,18 @@ from mask import *
 from transformers import AutoTokenizer, TFBertForMaskedLM
 
 inputs = {}
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
 def test_get_mask_token_index():
-    # assert get_mask_token_index(tokenizer.mask_token_id, tokenizer("then i picked up a [MASK] from the table.", return_tensors="tf")) == 6
-    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102])
+    inputs["input_ids"] = tf.constant([[101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102]])
     assert get_mask_token_index(tokenizer.mask_token_id, inputs) == 6
 
 def test_get_mask_token_index_1():
+    inputs["input_ids"] = tf.constant([[101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102]])
     assert get_mask_token_index(tokenizer.mask_token_id, tokenizer("then i [MASK] up a book from the table.", return_tensors="tf")) == 2
 
 def test_get_mask_token_index_none():
-    assert get_mask_token_index(tokenizer.mask_token_id, tokenizer("then i picked up a book from the table.", return_tensors="tf")) == None
+    inputs["input_ids"] = tf.constant([[101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102]])
+    assert get_mask_token_index(tokenizer.mask_token_id, inputs) == None
 
 def test_get_color_for_attention_score_0():
     assert get_color_for_attention_score(tf.constant(0)) == (0, 0, 0)
