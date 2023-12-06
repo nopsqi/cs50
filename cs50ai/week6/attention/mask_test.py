b1f0c11b388cb2d0f6f617cb5dc0d8a593a7d0b1
Muhamamd Naufal Shidqi <nopal.sidqi@gmail.com>
2023-12-06 06:35:47 +0000

    Wed, Dec 6, 2023, 1:35 PM +07:00

--- cs50ai/week6/attention/mask_test.py
+++ cs50ai/week6/attention/mask_test.py
@@ -1,8 +1,13 @@
 from mask import *
+from tensorflow import dtypes
 
 inputs = {}
 
 def test_get_mask_token_index():
-    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102])
+    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102], dtype=dtypes.ml_dtypes.float8_e4m3b11fnuz)
     assert get_mask_token_index(103, inputs) == 6
 
+
+def test_get_mask_token_index():
+    inputs["input_ids"] = tf.constant([101, 2059, 1045, 3856, 2039, 1037, 103, 2013, 1996, 2795, 1012, 102])
+    assert get_mask_token_index(103, inputs) == 6
